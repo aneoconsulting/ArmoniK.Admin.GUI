4a58dfceb96e3070f2bdc76ef414e11867c94d5f
Estéban Soubiran <esoubiran@aneo.fr>
2022-04-22 15:45:05 +0200

    test(ui): header

--- libs/ui/src/lib/header/header.component.spec.ts
+++ libs/ui/src/lib/header/header.component.spec.ts
@@ -1,6 +1,14 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+
+@Component({
+  template: `<ui-header>
+    <div branding>title</div>
+  </ui-header>`,
+})
+class TestHostComponent {}
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +16,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HeaderComponent],
+      declarations: [HeaderComponent, TestHostComponent],
     }).compileComponents();
   });
 
@@ -20,5 +28,24 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have be a header', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('header')).toBeTruthy();
+  });
+
+  it('should have a class "branding"', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.branding')).toBeTruthy();
+  });
+
+  it('should have a div[branding] with title"', () => {
+    const fixture = TestBed.createComponent(TestHostComponent);
+    const header = fixture.nativeElement;
+
+    expect(header.querySelector('div[branding]').textContent).toContain(
+      'title'
+    );
   });
 });
