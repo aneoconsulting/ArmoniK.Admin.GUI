3d7dcc958047416b8282ad17d813ce2097a52ac3
Estéban Soubiran <esoubiran@aneo.fr>
2022-04-25 12:29:16 +0200

    fix(shell): update selector in pages

--- apps/app/src/app/modules/pages/modules/sessions/sessions.component.ts
+++ apps/app/src/app/modules/pages/modules/sessions/sessions.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-sessions',
+  selector: 'app-pages-sessions',
   templateUrl: './sessions.component.html',
   styleUrls: ['./sessions.component.scss'],
 })
