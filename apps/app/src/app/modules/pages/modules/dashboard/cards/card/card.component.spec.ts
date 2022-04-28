632859f5686a8721c1c21c41535862b9c6eb8da1
Estéban Soubiran <esoubiran@aneo.fr>
2022-04-28 11:41:00 +0200

    test(dashboard): add test to card

--- apps/app/src/app/modules/pages/modules/dashboard/cards/card/card.component.spec.ts
+++ apps/app/src/app/modules/pages/modules/dashboard/cards/card/card.component.spec.ts
@@ -1,6 +1,15 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
+
+@Component({
+  template: `<app-pages-dashboard-cards-card>
+    <span title>title</span>
+    <span link>link</span>
+  </app-pages-dashboard-cards-card>`,
+})
+class TestHostComponent {}
 
 describe('CardComponent', () => {
   let component: CardComponent;
@@ -8,7 +17,7 @@ describe('CardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CardComponent],
+      declarations: [CardComponent, TestHostComponent],
     }).compileComponents();
   });
 
@@ -20,5 +29,41 @@ describe('CardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be an article with class "card"', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('article.card')).toBeTruthy();
+  });
+
+  it('should have a h3 with class "card-title"', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h3.card-title')).toBeTruthy();
+  });
+
+  it('should have a footer with class "card-footer"', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('footer.card-footer')).toBeTruthy();
+  });
+
+  it('should have a card-block before the footer', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.card :first-child').classList).toContain(
+      'card-block'
+    );
+  });
+
+  it('should have a span[title] with title"', () => {
+    const fixture = TestBed.createComponent(TestHostComponent);
+    const card = fixture.nativeElement;
+
+    expect(card.querySelector('span[title]').textContent).toContain('title');
+  });
+
+  it('should have a span[link] with link"', () => {
+    const fixture = TestBed.createComponent(TestHostComponent);
+    const card = fixture.nativeElement;
+
+    expect(card.querySelector('span[link]').textContent).toContain('link');
   });
 });
