8edd4ba7eb8edc6c83d9d04a14cd0139d63ac7eb
Estéban Soubiran <esoubiran@aneo.fr>
2022-04-29 10:33:25 +0200

    fix(tasks): remove unused code

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@armonik.admin.gui/interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-
-  constructor(private http: HttpClient) {}
-}
+export class AppComponent {}
