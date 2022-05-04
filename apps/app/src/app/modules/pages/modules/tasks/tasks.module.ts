0302d402e9a4598f6a9a4dcf4f26efe655456f00
Estéban Soubiran <esoubiran@aneo.fr>
2022-05-04 16:57:00 +0200

    feat(tasks): generate templates

---
+++ apps/app/src/app/modules/pages/modules/tasks/tasks.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TasksComponent } from './tasks.component';
+
+@NgModule({
+  declarations: [TasksComponent],
+  imports: [CommonModule],
+})
+export class TasksModule {}
