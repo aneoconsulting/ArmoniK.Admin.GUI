89d96942b8445e4eeaf28a580bc4e8127fc7fe77
Estéban Soubiran <esoubiran@aneo.fr>
2022-05-04 16:47:23 +0200

    feat(applications): generate templates

---
+++ apps/app/src/app/modules/pages/modules/applications/applications.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pages-applications',
+  templateUrl: './applications.component.html',
+  styleUrls: ['./applications.component.scss'],
+})
+export class ApplicationsComponent {}
---
+++ apps/app/src/app/modules/pages/modules/applications/applications.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApplicationsComponent } from './applications.component';
+
+@NgModule({
+  declarations: [ApplicationsComponent],
+  imports: [CommonModule],
+})
+export class ApplicationsModule {}
