f923fa63197402e0b73c2644fb2a5d01e114aa34
Estéban Soubiran <esoubiran@aneo.fr>
2022-05-04 11:27:02 +0200

    fix(ui): remove inccorect code

--- libs/ui/src/lib/sidenav/sidenav.component.spec.ts
+++ libs/ui/src/lib/sidenav/sidenav.component.spec.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ClarityModule } from '@clr/angular';
 
 import { SidenavComponent } from './sidenav.component';
 
@@ -15,6 +16,7 @@ describe('SidenavComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SidenavComponent, TestHostComponent],
+      imports: [ClarityModule],
     }).compileComponents();
   });
 
@@ -26,29 +28,5 @@ describe('SidenavComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should be a nav', () => {
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('nav')).toBeTruthy();
-  });
-
-  it('should add a class "sidenav" to host', () => {
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.classList).toContain('sidenav');
-  });
-
-  it('should have a "section.sidenav-content"', () => {
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('section.sidenav-content')).toBeTruthy();
-  });
-
-  it('should have a "section.sidenav-content" with "links"', () => {
-    const fixture = TestBed.createComponent(TestHostComponent);
-    const sidenav = fixture.nativeElement;
-
-    expect(
-      sidenav.querySelector('section.sidenav-content').textContent
-    ).toContain('links');
   });
 });
