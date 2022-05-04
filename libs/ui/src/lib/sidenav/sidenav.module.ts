2ef3621f9fe7255ddfa987555a3350f00a695794
Estéban <103997512+esoubiran-aneo@users.noreply.github.com>
2022-05-04 11:47:00 +0200

    Update UI from sidenav (#14)
    * feat(ui): update sidenav
    
    * fix(ui): components order
    
    * fix(ui): remove inccorect code
    
    * test: remove unused module

--- libs/ui/src/lib/sidenav/sidenav.module.ts
+++ libs/ui/src/lib/sidenav/sidenav.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidenavComponent } from './sidenav.component';
-
+import { ClarityModule } from '@clr/angular';
 @NgModule({
   declarations: [SidenavComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ClarityModule],
   exports: [SidenavComponent],
 })
 export class SidenavModule {}
