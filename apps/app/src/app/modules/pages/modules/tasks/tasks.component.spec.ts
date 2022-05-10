b33424e48c608cf0c9fd4c51030924e3ce223773
Estéban Soubiran <esoubiran@aneo.fr>
2022-05-10 15:08:53 +0200

    test: pass

--- apps/app/src/app/modules/pages/modules/tasks/tasks.component.spec.ts
+++ apps/app/src/app/modules/pages/modules/tasks/tasks.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { TasksComponent } from './tasks.component';
 
 describe('TasksComponent', () => {
@@ -9,6 +9,7 @@ describe('TasksComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TasksComponent],
+      imports: [TranslateModule.forRoot()],
     }).compileComponents();
   });
 
