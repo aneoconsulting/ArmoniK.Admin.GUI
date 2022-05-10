0ea7977bf62067394cf6eada904a729d0b86e455
Estéban Soubiran <esoubiran@aneo.fr>
2022-05-10 14:47:21 +0200

    feat(title): update title using translation

--- apps/app/src/app/modules/pages/modules/applications/applications.module.ts
+++ apps/app/src/app/modules/pages/modules/applications/applications.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApplicationsComponent } from './applications.component';
 import { RouterModule } from '@angular/router';
+import { TitleService } from '../../../core/services';
+import { TranslateService } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [ApplicationsComponent],
@@ -10,4 +12,13 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([{ path: '', component: ApplicationsComponent }]),
   ],
 })
-export class ApplicationsModule {}
+export class ApplicationsModule {
+  constructor(
+    private titleService: TitleService,
+    private translateService: TranslateService
+  ) {
+    this.titleService.setTitle(
+      this.translateService.instant('applications.title')
+    );
+  }
+}
