09f06d9e09b448b99026772d1643e2192c2e7ba2
Estéban Soubiran <esoubiran@aneo.fr>
2022-05-10 14:39:47 +0200

    feat(title): add page title

--- apps/app/src/app/modules/pages/modules/tasks/tasks.component.ts
+++ apps/app/src/app/modules/pages/modules/tasks/tasks.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { TitleService } from '../../../core/services';
 
 @Component({
   selector: 'app-pages-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss'],
 })
-export class TasksComponent {}
+export class TasksComponent {
+  constructor(private titleService: TitleService) {
+    this.titleService.setTitle('Tasks');
+  }
+}
