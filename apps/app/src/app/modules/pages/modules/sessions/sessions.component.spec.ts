f1493247cb1f84bbd859443ae962b3af49a51921
Estéban Soubiran <esoubiran@aneo.fr>
2022-05-11 16:02:11 +0200

    test: pass

--- apps/app/src/app/modules/pages/modules/sessions/sessions.component.spec.ts
+++ apps/app/src/app/modules/pages/modules/sessions/sessions.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -10,7 +11,7 @@ describe('SessionsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SessionsComponent],
-      imports: [TranslateModule.forRoot()],
+      imports: [TranslateModule.forRoot(), HttpClientModule],
     }).compileComponents();
   });
 
