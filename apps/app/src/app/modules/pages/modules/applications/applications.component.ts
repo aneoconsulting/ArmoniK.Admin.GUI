1b07d92e764eaaf6918281af2b49308329313fe8
Estéban Soubiran <esoubiran@aneo.fr>
2022-05-16 11:30:47 +0200

    fix: remove unused code

--- apps/app/src/app/modules/pages/modules/applications/applications.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-pages-applications',
-  templateUrl: './applications.component.html',
-  styleUrls: ['./applications.component.scss'],
-})
-export class ApplicationsComponent {}
