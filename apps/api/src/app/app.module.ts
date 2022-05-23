9dfb2e83ddb7a3a401962e755e3bfafc87af5272
Estéban Soubiran <esoubiran@aneo.fr>
2022-05-23 10:20:29 +0200

    docs(mongo): remove unused code for the pr

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { SessionsModule } from './sessions/sessions.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { mongoConnectionString } from '../mongo-client.options';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(mongoConnectionString()),
-    SessionsModule,
   ],
   controllers: [],
   providers: [],
