eb2535bbce455a47c2d14d86cb9a527f84af04a7
Estéban Soubiran <esoubiran@aneo.fr>
2022-05-23 10:02:32 +0200

    feat(gui-sessions-ui): update datagrid

--- apps/app/src/app/modules/pages/modules/sessions/sessions.component.ts
+++ apps/app/src/app/modules/pages/modules/sessions/sessions.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ClrDatagridStateInterface } from '@clr/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { Session } from '../../../core/entities';
 import { TitleService } from '../../../core/services/title.service';
@@ -9,6 +10,9 @@ import { TitleService } from '../../../core/services/title.service';
   styleUrls: ['./sessions.component.scss'],
 })
 export class SessionsComponent {
+  total = 0;
+  loading = true;
+
   sessions: Session[] = [
     {
       id: 1,
@@ -37,9 +41,15 @@ export class SessionsComponent {
     private translateService: TranslateService
   ) {
     this.titleService.setTitle(this.translateService.instant('sessions.title'));
+    this.total = this.sessions.length;
+    this.loading = false;
   }
 
   trackBySession(_: number, session: Session): number {
     return session.id;
+  }
+
+  refresh(state: ClrDatagridStateInterface) {
+    console.log(state);
   }
 }
