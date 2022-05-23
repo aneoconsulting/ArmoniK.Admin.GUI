d107e168d5f401f27decce4958aad631e7112ffd
Estéban Soubiran <esoubiran@aneo.fr>
2022-05-23 12:07:01 +0200

    docs(default_api): add comment

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -2,6 +2,9 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  /**
+   * Used to view a message to check if the API is up and running.
+   */
   @Get()
   index(): { message: string } {
     return { message: 'Welcome to ArmoniK GUI API' };
