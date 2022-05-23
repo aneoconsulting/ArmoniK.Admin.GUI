32c71b61c1111aeecc350460af597aa7094ec75e
Estéban Soubiran <esoubiran@aneo.fr>
2022-05-23 10:24:07 +0200

    docs(scripts): add comments

--- scripts/create-containers.sh
+++ scripts/create-containers.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# $1 is for the name of the app to build (app or api)
+# $2 is for the version of the container (eg: 1.0.0 or develop)
 docker build -f "$1.Dockerfile" -t "dockerhubaneo/armonik_admin_$1:$2" .
