e077d69103912a846406b3309499b94a831089f9
Estéban Soubiran <esoubiran@aneo.fr>
2022-06-01 17:37:14 +0200

    ci: lint

--- apps/app/src/app/modules/pages/sessions/pages/session-detail/session-detail.module.ts
+++ apps/app/src/app/modules/pages/sessions/pages/session-detail/session-detail.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { SharedModule } from 'apps/app/src/app/shared';
+import { SharedModule } from '../../../../../shared';
 import { TasksListComponent } from './components';
 import { TaskDetailComponent } from './pages';
 import { SessionDetailRoutingModule } from './session-detail-routing.module';
