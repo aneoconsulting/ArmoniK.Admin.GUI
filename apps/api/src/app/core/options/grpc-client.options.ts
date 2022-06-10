65bb1d7acecbe38c09bfe48634c594acd46a549e
Estéban Soubiran <esoubiran@aneo.fr>
2022-06-10 09:36:43 +0200

    feat: use submodule for proto

--- apps/api/src/app/core/options/grpc-client.options.ts
+++ apps/api/src/app/core/options/grpc-client.options.ts
@@ -5,7 +5,10 @@ export const grpcClientOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
     package: 'ArmoniK.api.grpc.v1',
-    protoPath: join(__dirname, './app/core/proto/submitter_service.proto'),
+    protoPath: join(
+      __dirname,
+      './assets/ArmoniK.Api/Protos/V1/submitter_service.proto'
+    ),
     url: process.env.ControlPlane__Endpoint?.replace(/^https?:\/\//, ''),
   },
 };
