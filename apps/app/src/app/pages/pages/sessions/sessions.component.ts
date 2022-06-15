45b9c7703da20399f392f99b30fc5978106fa46c
Estéban Soubiran <esoubiran@aneo.fr>
2022-06-15 14:36:48 +0200

    fix: remove log

--- apps/app/src/app/pages/pages/sessions/sessions.component.ts
+++ apps/app/src/app/pages/pages/sessions/sessions.component.ts
@@ -43,7 +43,6 @@ export class SessionsComponent implements OnInit {
    * @param state
    */
   onRefreshSessions(state: ClrDatagridStateInterface) {
-    console.log('refresh', state);
     this.loadingSessions = true;
 
     const nextPage = state?.page?.current ?? 1;
