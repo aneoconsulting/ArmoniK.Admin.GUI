18c5a0871eb8509025a22bf02f9bc760e02fea35
Estéban Soubiran <esoubiran@aneo.fr>
2022-06-17 14:03:35 +0200

    feat: update grpc status code

--- apps/api/src/app/core/services/grpc-error.service.ts
+++ apps/api/src/app/core/services/grpc-error.service.ts
@@ -4,12 +4,13 @@ import {
   NotFoundException,
 } from '@nestjs/common';
 import { Observable, ObservableInput } from 'rxjs';
+import { status } from '@grpc/grpc-js';
 
 @Injectable()
 export class GrpcErrorService {
   handleError(error, _: Observable<unknown>): ObservableInput<never> {
     switch (error.code) {
-      case 5:
+      case status.NOT_FOUND:
         throw new NotFoundException();
       default:
         throw new InternalServerErrorException(error.message);
