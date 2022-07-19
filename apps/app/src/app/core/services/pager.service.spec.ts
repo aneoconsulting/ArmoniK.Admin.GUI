dd8d7d44c75e3b1564e0d40ee781f7a665999926
Estéban Soubiran <esoubiran@aneo.fr>
2022-07-19 13:42:19 +0200

    test: update using review

--- apps/app/src/app/core/services/pager.service.spec.ts
+++ apps/app/src/app/core/services/pager.service.spec.ts
@@ -15,58 +15,64 @@ describe('PagerService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should use defaultNextPage', () => {
-    const params = service.createHttpParams({});
+  describe('page', () => {
+    it('should use defaultNextPage set to 1', () => {
+      const params = service.createHttpParams({});
 
-    expect(params.get('page')).toEqual('1');
-  });
-
-  it('should use defaultLimit', () => {
-    const params = service.createHttpParams({});
-
-    expect(params.get('limit')).toEqual('10');
-  });
-
-  it('should use state page and limit', () => {
-    const params = service.createHttpParams({
-      page: {
-        current: 2,
-        size: 20,
-      },
+      expect(params.get('page')).toEqual('1');
     });
 
-    expect(params.get('page')).toEqual('2');
-    expect(params.get('limit')).toEqual('20');
-  });
+    it('should use defaultLimit set to 10', () => {
+      const params = service.createHttpParams({});
 
-  it('should use state sort', () => {
-    const params = service.createHttpParams({
-      sort: {
-        by: 'name',
-        reverse: true,
-      },
+      expect(params.get('limit')).toEqual('10');
     });
 
-    expect(params.get('orderBy')).toEqual('name');
-    expect(params.get('order')).toEqual('-1');
-  });
-
-  it('should use state sort only if by is defined', () => {
-    const params = service.createHttpParams({});
-
-    expect(params.get('sort')).toEqual(null);
-  });
-
-  it('should use state filters', () => {
-    const params = service.createHttpParams({
-      filters: [
-        {
-          property: 'name',
-          value: 'test',
+    it('should use state page and limit', () => {
+      const params = service.createHttpParams({
+        page: {
+          current: 2,
+          size: 20,
         },
-      ],
+      });
+
+      expect(params.get('page')).toEqual('2');
+      expect(params.get('limit')).toEqual('20');
+    });
+  });
+
+  describe('sort', () => {
+    it('should use state sort', () => {
+      const params = service.createHttpParams({
+        sort: {
+          by: 'name',
+          reverse: true,
+        },
+      });
+
+      expect(params.get('orderBy')).toEqual('name');
+      expect(params.get('order')).toEqual('-1');
     });
 
-    expect(params.get('name')).toEqual('test');
+    it('should use state sort only if by is defined', () => {
+      const params = service.createHttpParams({});
+
+      expect(params.get('sort')).toEqual(null);
+    });
+  });
+
+  describe('filters', () => {
+    it('should use state filters', () => {
+      const params = service.createHttpParams({
+        filters: [
+          {
+            property: 'name',
+            value: 'test',
+          },
+        ],
+      });
+
+      expect(params.get('name')).toEqual('test');
+    });
   });
 });
