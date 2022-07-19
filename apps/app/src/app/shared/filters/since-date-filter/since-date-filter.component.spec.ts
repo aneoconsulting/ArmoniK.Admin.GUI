046548d01728c6fb623aa2afe0651c6f1b0d640e
Estéban Soubiran <esoubiran@aneo.fr>
2022-07-19 13:24:10 +0200

    test: update using review

--- apps/app/src/app/shared/filters/since-date-filter/since-date-filter.component.spec.ts
+++ apps/app/src/app/shared/filters/since-date-filter/since-date-filter.component.spec.ts
@@ -25,13 +25,49 @@ describe('SinceDateFilterComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have a property "name"', () => {
-    expect(component.name).toBeDefined();
+  describe('input', () => {
+    it('should have a label', () => {
+      const label = component.label;
+      expect(label).toBeDefined();
+    });
+
+    it('should have a default text', () => {
+      const defaultText = component.defaultText;
+      expect(defaultText).toBeDefined();
+    });
+
+    it('should have a text', () => {
+      const text = component.text;
+      expect(text).toBeDefined();
+    });
+
+    it('should have a name', () => {
+      const name = component.name;
+      expect(name).toBeDefined();
+    });
+
+    it('should have a default value set to 7', () => {
+      const defaultValue = component.defaultValue;
+      expect(defaultValue).toBe(7);
+    });
   });
 
   it('should return the name with "property"', () => {
     component.name = 'test';
     expect(component.property).toBe(component.name);
+  });
+
+  it('should always return true for accepts', () => {
+    expect(component.accepts()).toBeTruthy();
+  });
+
+  it('should return false when is not active', () => {
+    expect(component.isActive()).toBeFalsy();
+  });
+
+  it('should return true when is active', () => {
+    component.selectedValue = 'test';
+    expect(component.isActive()).toBeTruthy();
   });
 
   it('should emit an event when the value changes', () => {
@@ -51,28 +87,33 @@ describe('SinceDateFilterComponent', () => {
     expect(component.value).toBe('test');
   });
 
-  it('should have a clr-select-container', () => {
-    expect(
-      fixture.nativeElement.querySelector('clr-select-container')
-    ).toBeTruthy();
+  describe('date since', () => {
+    it('should create a new date if params is not defined', () => {
+      const date = component.createDateSince();
+      expect(date).toBeDefined();
+    });
+
+    it('should create a new date subtracting the number of days from now', () => {
+      const date = component.createDateSince(7);
+      expect(date.getUTCDate()).toBe(new Date().getUTCDate() - 7);
+    });
   });
 
-  it('should have a label and a select element', () => {
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('label')).toBeTruthy();
-  });
+  describe('ui', () => {
+    it('should have a clr-select-container', () => {
+      expect(
+        fixture.nativeElement.querySelector('clr-select-container')
+      ).toBeTruthy();
+    });
 
-  it('should display a list of options', () => {
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('select')).toBeTruthy();
-  });
+    it('should have a label and a select element', () => {
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('label')).toBeTruthy();
+    });
 
-  it('should always return true for accepts', () => {
-    expect(component.accepts()).toBeTruthy();
-  });
-
-  it('should create a new date if params is not defined', () => {
-    const date = component.createDateSince();
-    expect(date).toBeDefined();
+    it('should display a list of options', () => {
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('select')).toBeTruthy();
+    });
   });
 });
