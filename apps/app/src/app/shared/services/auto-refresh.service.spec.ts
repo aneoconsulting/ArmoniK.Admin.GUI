44ae3b45adfe0ad9eb7f5747b93f2a6afa20355e
Estéban Soubiran <esoubiran@aneo.fr>
2022-07-19 13:11:44 +0200

    test: use describe to order tests

--- apps/app/src/app/shared/services/auto-refresh.service.spec.ts
+++ apps/app/src/app/shared/services/auto-refresh.service.spec.ts
@@ -20,10 +20,10 @@ describe('AutoRefreshService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should call "disable" when "ngOnDestroy" is called', () => {
-    service.enable();
-    service.ngOnDestroy();
-    expect(service.interval).toBeNull();
+  it('should throw an error when fn is call when fn is not set', () => {
+    expect(function () {
+      service.fn();
+    }).toThrowError('AutoRefreshService.fn is not set');
   });
 
   it('should set new timer when "setTimer" is called', () => {
@@ -31,69 +31,77 @@ describe('AutoRefreshService', () => {
     expect(service.timer).toEqual(10_000);
   });
 
-  it('should restart interval when "setTimer" is called and interval enabled', () => {
-    service.interval = setInterval(() => null, 10_000);
-    const initialInterval = service.interval;
-    service.setTimer(10_000);
-    expect(service.interval).toBeTruthy();
-    // Must be different instance (restarted with new timer)
-    expect(service.interval).not.toEqual(initialInterval);
+  describe('interval', () => {
+    it('should not be enabled when interval is not set', () => {
+      service.interval = null;
+      expect(service.isEnabled()).toBeFalsy();
+    });
+
+    it('should be enabled when interval is set', () => {
+      service.interval = setInterval(() => null, 10_000);
+      expect(service.isEnabled()).toBeTruthy();
+    });
+
+    it('should restart when "setTimer" is called and interval enabled', () => {
+      service.interval = setInterval(() => null, 10_000);
+      const initialInterval = service.interval;
+      service.setTimer(10_000);
+      expect(service.interval).toBeTruthy();
+      // Must be different instance (restarted with new timer)
+      expect(service.interval).not.toEqual(initialInterval);
+    });
+
+    it('should not restart when "setTimer" is called and interval disabled', () => {
+      service.interval = null;
+      service.setTimer(10_000);
+      expect(service.interval).toBeNull();
+    });
+
+    it('should toggle when "toggle" is called', () => {
+      service.interval = null;
+      service.toggle();
+      expect(service.interval).toBeTruthy();
+      service.toggle();
+      expect(service.interval).toBeNull();
+    });
   });
 
-  it('should not restart interval when "setTimer" is called and interval disabled', () => {
-    service.interval = null;
-    service.setTimer(10_000);
-    expect(service.interval).toBeNull();
+  describe('enable', () => {
+    it('should enable interval when "enable" is called', () => {
+      service.interval = null;
+      service.enable();
+      expect(service.interval).toBeTruthy();
+    });
   });
 
-  it('should enable interval when "enable" is called', () => {
-    service.interval = null;
-    service.enable();
-    expect(service.interval).toBeTruthy();
+  describe('disable', () => {
+    it('should remove interval when "disable" is called', () => {
+      service.interval = setInterval(() => null, 10_000);
+      service.disable();
+      expect(service.interval).toBeNull();
+    });
+
+    it('should clear interval when "disable" is called', () => {
+      service.interval = setInterval(() => null, 10_000);
+      const intervalId = service.interval;
+      spyOn(window, 'clearInterval');
+      service.disable();
+      expect(window.clearInterval).toHaveBeenCalledWith(intervalId);
+    });
   });
 
-  it('should disable interval when "disable" is called', () => {
-    service.interval = setInterval(() => null, 10_000);
-    service.disable();
-    expect(service.interval).toBeNull();
-  });
+  describe('interval', () => {
+    it('should set be null when navigate to another route', fakeAsync(() => {
+      service.enable();
+      router.navigate(['/']);
+      tick();
+      expect(service.interval).toBeNull();
+    }));
 
-  it('should clear interval when "disable" is called', () => {
-    service.interval = setInterval(() => null, 10_000);
-    const intervalId = service.interval;
-    spyOn(window, 'clearInterval');
-    service.disable();
-    expect(window.clearInterval).toHaveBeenCalledWith(intervalId);
+    it('should set be null when component is destroy', () => {
+      service.enable();
+      service.ngOnDestroy();
+      expect(service.interval).toBeNull();
+    });
   });
-
-  it('should not be enabled when interval is not set', () => {
-    service.interval = null;
-    expect(service.isEnabled()).toBeFalsy();
-  });
-
-  it('should be enabled when interval is set', () => {
-    service.interval = setInterval(() => null, 10_000);
-    expect(service.isEnabled()).toBeTruthy();
-  });
-
-  it('should toggle interval when "toggle" is called', () => {
-    service.interval = null;
-    service.toggle();
-    expect(service.interval).toBeTruthy();
-    service.toggle();
-    expect(service.interval).toBeNull();
-  });
-
-  it('should throw an error when fn is call when fn is not set', () => {
-    expect(function () {
-      service.fn();
-    }).toThrowError('AutoRefreshService.fn is not set');
-  });
-
-  it('should disable interval on routing', fakeAsync(() => {
-    service.enable();
-    router.navigate(['/']);
-    tick();
-    expect(service.interval).toBeNull();
-  }));
 });
