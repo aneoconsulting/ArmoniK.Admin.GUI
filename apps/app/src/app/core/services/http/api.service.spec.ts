8d986e3221380c1b579cea7e8189c8783cf6155e
Estéban Soubiran <esoubiran@aneo.fr>
2022-07-19 13:59:21 +0200

    test: update using review

--- apps/app/src/app/core/services/http/api.service.spec.ts
+++ apps/app/src/app/core/services/http/api.service.spec.ts
@@ -1,92 +1,118 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { of, throwError } from 'rxjs';
+import { of, Subscription, throwError } from 'rxjs';
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
   let service: ApiService;
   let http: jasmine.SpyObj<HttpClient>;
+  let subscription: Subscription;
 
   beforeEach(() => {
     http = jasmine.createSpyObj('HttpClient', ['get', 'put']);
     service = new ApiService(http);
   });
 
+  afterEach(() => {
+    if (subscription) {
+      subscription.unsubscribe();
+    }
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should get', (done) => {
-    const expectedData = { name: 'test' };
-    http.get.and.returnValue(of(expectedData));
+  describe('get', () => {
+    it('should get', (done) => {
+      const expectedData = { name: 'test' };
+      http.get.and.returnValue(of(expectedData));
 
-    const path = 'test';
-    const params = new HttpParams();
+      const path = 'test';
+      const params = new HttpParams();
 
-    service.get(path, params).subscribe(
-      (data) => {
-        expect(data).toEqual(expectedData);
-        done();
-      },
-      (_) => {
-        done.fail();
-      }
-    );
+      subscription = service.get(path, params).subscribe({
+        next: (data) => {
+          expect(data).toEqual(expectedData);
+          done();
+        },
+        error: () => {
+          done.fail();
+        },
+      });
 
-    expect(http.get.calls.count()).toBe(1);
+      expect(http.get.calls.count()).toBe(1);
+    });
+
+    it('should return an error if the request fails with get', (done) => {
+      const error = { status: 404 };
+      http.get.and.returnValue(throwError(() => error));
+
+      const path = 'test';
+      const params = new HttpParams();
+
+      subscription = service.get(path, params).subscribe({
+        next: () => done.fail('expected an error'),
+        error: (err) => {
+          expect(err).toEqual(error);
+          done();
+        },
+      });
+    });
   });
 
-  it('should put with a body', (done) => {
-    const expectedData = { name: 'test' };
-    http.put.and.returnValue(of(expectedData));
+  describe('put', () => {
+    it('should put with a body', (done) => {
+      const expectedData = { name: 'test' };
+      http.put.and.returnValue(of(expectedData));
 
-    const path = 'test';
-    const body = { name: 'test' };
+      const path = 'test';
+      const body = { name: 'test' };
 
-    service.put(path, body).subscribe(
-      (data) => {
-        expect(data).toEqual(expectedData);
-        done();
-      },
-      (_) => {
-        done.fail();
-      }
-    );
+      subscription = service.put(path, body).subscribe({
+        next: (data) => {
+          expect(data).toEqual(expectedData);
+          done();
+        },
+        error: () => {
+          done.fail();
+        },
+      });
 
-    expect(http.put.calls.count()).toBe(1);
-  });
+      expect(http.put.calls.count()).toBe(1);
+    });
 
-  it('should put without a body', (done) => {
-    const expectedData = { name: 'test' };
-    http.put.and.returnValue(of(expectedData));
+    it('should put without a body', (done) => {
+      const expectedData = { name: 'test' };
+      http.put.and.returnValue(of(expectedData));
 
-    const path = 'test';
+      const path = 'test';
 
-    service.put(path).subscribe(
-      (data) => {
-        expect(data).toEqual(expectedData);
-        done();
-      },
-      (_) => {
-        done.fail();
-      }
-    );
+      subscription = service.put(path).subscribe({
+        next: (data) => {
+          expect(data).toEqual(expectedData);
+          done();
+        },
+        error: () => {
+          done.fail();
+        },
+      });
 
-    expect(http.put.calls.count()).toBe(1);
-  });
+      expect(http.put.calls.count()).toBe(1);
+    });
 
-  it('should return an error if the request fails with get', (done) => {
-    const error = { status: 404 };
-    http.get.and.returnValue(throwError(() => error));
+    it('should return an error if the request fails with PUT', (done) => {
+      const error = { status: 404 };
+      http.put.and.returnValue(throwError(() => error));
 
-    const path = 'test';
-    const params = new HttpParams();
+      const path = 'test';
 
-    service.get(path, params).subscribe({
-      next: (_) => done.fail('expected an error'),
-      error: (err) => {
-        expect(err).toEqual(error);
-        done();
-      },
+      subscription = service.put(path).subscribe({
+        next: () => done.fail('expected an error'),
+        error: (err) => {
+          expect(err).toEqual(error);
+          done();
+        },
+      });
     });
   });
 });
