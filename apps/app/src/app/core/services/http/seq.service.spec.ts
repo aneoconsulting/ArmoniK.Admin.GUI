c53dcbfc0bff69c24bb86730ed0ed9a30066bd2d
Estéban Soubiran <esoubiran@aneo.fr>
2022-07-19 17:19:35 +0200

    test: update using review

--- apps/app/src/app/core/services/http/seq.service.spec.ts
+++ apps/app/src/app/core/services/http/seq.service.spec.ts
@@ -1,47 +1,56 @@
-import { of, throwError } from 'rxjs';
+import { of, Subscription, throwError } from 'rxjs';
 import { ApiService } from './api.service';
 import { SeqService } from './seq.service';
 
 describe('SeqService', () => {
   let service: SeqService;
   let http: jasmine.SpyObj<ApiService>;
+  let subscription: Subscription;
 
   beforeEach(() => {
     http = jasmine.createSpyObj('ApiService', ['get']);
     service = new SeqService(http);
   });
 
+  afterEach(() => {
+    if (subscription) {
+      subscription.unsubscribe();
+    }
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should ping', (done) => {
-    http.get.and.returnValue(of({}));
+  describe('ping', () => {
+    it('should ping', (done) => {
+      http.get.and.returnValue(of({}));
 
-    service.ping().subscribe({
-      next: () => {
-        expect(http.get).toHaveBeenCalledWith('/api/seq/ping');
-        done();
-      },
-      error: () => {
-        done.fail('should not be called');
-      },
+      subscription = service.ping().subscribe({
+        next: () => {
+          expect(http.get).toHaveBeenCalledWith('/api/seq/ping');
+          done();
+        },
+        error: () => {
+          done.fail('should not be called');
+        },
+      });
     });
-  });
 
-  it('should ping error', (done) => {
-    const error = { status: 404 };
+    it('should ping error', (done) => {
+      const error = { status: 404 };
 
-    http.get.and.returnValue(throwError(() => error));
+      http.get.and.returnValue(throwError(() => error));
 
-    service.ping().subscribe({
-      next: () => {
-        done.fail('should not be called');
-      },
-      error: (err) => {
-        expect(err).toEqual(error);
-        done();
-      },
+      subscription = service.ping().subscribe({
+        next: () => {
+          done.fail('should not be called');
+        },
+        error: (err) => {
+          expect(err).toEqual(error);
+          done();
+        },
+      });
     });
   });
 });
