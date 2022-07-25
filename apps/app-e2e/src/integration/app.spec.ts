1935040ff454e481f65f37836e1710339be9d515
Estéban Soubiran <esoubiran@aneo.fr>
2022-07-25 09:40:05 +0200

    fix: reload on language change

--- apps/app-e2e/src/integration/app.spec.ts
+++ apps/app-e2e/src/integration/app.spec.ts
@@ -5,4 +5,14 @@ describe('app', () => {
     // check if url is /admin/dashboard
     cy.url().should('include', '/dashboard');
   });
+
+  it('should display the date on the header', () => {
+    cy.get('.title--date').contains(/AM|PM/);
+  });
+
+  it('should change the date on the header when passing in FR', () => {
+    cy.get(':nth-child(2) > .btn').click();
+    cy.get('.title--date').contains(/\d{2}\/\d{2}\/\d{4} (\d{2}:\d{2})/);
+    cy.get(':nth-child(1) > .btn').click();
+  });
 });
