b26aa8be4cf083d00988009baa27541919cf313e
Estéban Soubiran <esoubiran@aneo.fr>
2022-07-29 11:49:54 +0200

    fix: update path to logo

--- apps/app/src/app/pages/pages.component.ts
+++ apps/app/src/app/pages/pages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Application } from '@armonik.admin.gui/armonik-typing';
+import { environment } from '../../environments/environment';
 import {
   LanguageService,
   Language,
