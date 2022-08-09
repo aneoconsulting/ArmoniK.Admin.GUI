cb07d2810bc63d3008c86390f65b782316761bfe
Estéban Soubiran <esoubiran@aneo.fr>
2022-08-09 12:29:34 +0200

    feat: create how to use page

---
+++ apps/app/src/app/pages/pages/how-to-use/how-to-use.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pages-how-to-use',
+  templateUrl: './how-to-use.component.html',
+  styleUrls: ['./how-to-use.component.scss'],
+})
+export class HowToUseComponent {}
