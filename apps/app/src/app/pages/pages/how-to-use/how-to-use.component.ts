c0687ede24dcc2e0967f09b2ea50ad986237f125
Estéban Soubiran <esoubiran@aneo.fr>
2022-08-09 14:15:38 +0200

    feat: translate to en

--- apps/app/src/app/pages/pages/how-to-use/how-to-use.component.ts
+++ apps/app/src/app/pages/pages/how-to-use/how-to-use.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { BrowserTitleService, LanguageService } from '../../../core';
+import {
+  BrowserTitleService,
+  LanguageCode,
+  LanguageService,
+} from '../../../core';
 
 @Component({
   selector: 'app-pages-how-to-use',
@@ -16,5 +20,13 @@ export class HowToUseComponent implements OnInit {
     this.browserTitleService.setTitle(
       this.languageService.instant('navigation.how_to_use')
     );
+  }
+
+  isFrench(): boolean {
+    return this.languageService.currentLang === LanguageCode.fr;
+  }
+
+  isEnglish(): boolean {
+    return this.languageService.currentLang === LanguageCode.en;
   }
 }
