2fd186e02086139920bf043c1eb5ad93eac71623
Estéban <103997512+esoubiran-aneo@users.noreply.github.com>
2022-08-09 16:37:57 +0200

    test: sessions (#212)
    * test: sessions
    
    * chore: organize import
    
    * test: udpate using review

--- apps/app/src/app/pages/pages/sessions/sessions.component.spec.ts
+++ apps/app/src/app/pages/pages/sessions/sessions.component.spec.ts
@@ -1,5 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import {
   FormattedSession,
@@ -8,29 +11,94 @@ import {
 import { UiModule } from '@armonik.admin.gui/ui';
 import { ClarityModule } from '@clr/angular';
 import { TranslateModule } from '@ngx-translate/core';
-import { PagerService, SessionsService } from '../../../core';
-import { AlertErrorComponent, SinceDateFilterComponent } from '../../../shared';
+import { of, throwError } from 'rxjs';
+import {
+  BrowserTitleService,
+  LanguageService,
+  PagerService,
+  Session,
+  SessionsService,
+} from '../../../core';
+import {
+  AlertErrorComponent,
+  AutoRefreshService,
+  SinceDateFilterComponent,
+} from '../../../shared';
 import { SessionsComponent } from './sessions.component';
 
+@Component({
+  selector: 'app-timer-interval-selector',
+  template: `<button class="timer-change" (click)="timerChange.emit(10)">
+    timer change
+  </button>`,
+})
+class MockTimerIntervalSelectorComponent {
+  @Input() timer = 0;
+  @Output() timerChange = new EventEmitter<number>();
+}
+
+@Component({
+  selector: 'app-auto-refresh-activator',
+  template: `<button
+    class="auto-refresh-change"
+    (click)="autoRefreshChange.emit()"
+  >
+    auto refresh change
+  </button>`,
+})
+class MockAutoRefreshActivatorComponent {
+  @Input() isEnabled = false;
+  @Output() autoRefreshChange = new EventEmitter();
+}
+
 describe('SessionsComponent', () => {
+  const sessionsPaginated = {
+    data: [
+      {
+        _id: '1',
+      },
+    ],
+    meta: {
+      total: 1,
+      perPage: 1,
+    },
+  } as unknown as Pagination<FormattedSession>;
+  let http: jasmine.SpyObj<SessionsService>;
   let component: SessionsComponent;
   let fixture: ComponentFixture<SessionsComponent>;
 
   beforeEach(async () => {
+    http = jasmine.createSpyObj('SessionsService', [
+      'getAllPaginated',
+      'cancel',
+    ]);
+
+    http.getAllPaginated.and.returnValue(of(sessionsPaginated));
+    http.cancel.and.returnValue(of({} as Session));
+
     await TestBed.configureTestingModule({
       declarations: [
         SessionsComponent,
         AlertErrorComponent,
         SinceDateFilterComponent,
+        MockTimerIntervalSelectorComponent,
+        MockAutoRefreshActivatorComponent,
       ],
       imports: [
+        BrowserAnimationsModule,
         RouterTestingModule.withRoutes([]),
         TranslateModule.forRoot(),
         HttpClientModule,
         ClarityModule,
         UiModule,
       ],
-      providers: [SessionsService, PagerService],
+      providers: [
+        { provide: SessionsService, useValue: http },
+        PagerService,
+        BrowserTitleService,
+        LanguageService,
+        AutoRefreshService,
+      ],
     }).compileComponents();
   });
 
@@ -44,229 +112,345 @@ describe('SessionsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have a error status for the first session', () => {
-    const sessions: Pagination<FormattedSession> = {
-      data: [
-        {
-          _id: '1',
-          status: 1,
-          countTasksError: 1,
-          countTasksCompleted: 1,
-          countTasksPending: 1,
-          countTasksProcessing: 1,
-          cancelledAt: '',
-          createdAt: '2020-01-01T00:00:00.000Z',
-        },
-      ],
-      meta: {
-        total: 1,
-        perPage: 10,
-        currentPage: 1,
-        nextPage: null,
-        prevPage: null,
-        firstPage: 1,
-        lastPage: 1,
-      },
-    };
+  describe('ngOnInit', () => {
+    it('should set refresh with correct fn', () => {
+      const spy = spyOn(component.autoRefreshService, 'setFn');
 
-    component.sessions = sessions;
-    fixture.detectChanges();
+      component.ngOnInit();
 
-    // select the first clr-dg-row
-    const row = fixture.nativeElement.querySelector('clr-dg-row');
-    // select the cell containing status
-    const cell = row.querySelectorAll('clr-dg-cell')[2];
-    // must only contains one child
-    expect(cell.childElementCount).toBe(1);
-    // child must contains the class .text-danger
-    expect(cell.children[0].classList.contains('text-danger')).toBeTruthy();
+      expect(spy).toHaveBeenCalled();
+
+      const fn = spy.calls.mostRecent().args[0];
+      fn();
+
+      expect(http.getAllPaginated).toHaveBeenCalled();
+    });
+
+    it('should set the browser title', () => {
+      const browserTitleService = TestBed.inject(BrowserTitleService);
+      const spy = spyOn(browserTitleService, 'setTitle');
+      component.ngOnInit();
+
+      expect(browserTitleService.setTitle).toHaveBeenCalled();
+      spy.calls.reset();
+    });
   });
 
-  it('should have a pending status for the first session', () => {
-    const sessions: Pagination<FormattedSession> = {
-      data: [
-        {
-          _id: '1',
-          status: 1,
-          countTasksError: 0,
-          countTasksCompleted: 0,
-          countTasksPending: 1,
-          countTasksProcessing: 0,
-          cancelledAt: '',
-          createdAt: '2020-01-01T00:00:00.000Z',
-        },
-      ],
-      meta: {
-        total: 1,
-        perPage: 10,
-        currentPage: 1,
-        nextPage: null,
-        prevPage: null,
-        firstPage: 1,
-        lastPage: 1,
-      },
-    };
+  describe('refresh', () => {
+    it('should call refresh', () => {
+      component.refresh();
 
-    component.sessions = sessions;
-    fixture.detectChanges();
-
-    // select the first clr-dg-row
-    const row = fixture.nativeElement.querySelector('clr-dg-row');
-    // select the cell containing status
-    const cell = row.querySelectorAll('clr-dg-cell')[2];
-    // must only contains one child
-    expect(cell.childElementCount).toBe(1);
-    // child must contains the class .text-pending
-    expect(cell.children[0].classList.contains('text-pending')).toBeTruthy();
+      expect(http.getAllPaginated).toHaveBeenCalled();
+    });
   });
 
-  it('should have a error status for the first session even with pending tasks', () => {
-    const sessions: Pagination<FormattedSession> = {
-      data: [
-        {
-          _id: '1',
-          status: 1,
-          countTasksError: 1,
-          countTasksCompleted: 0,
-          countTasksPending: 2,
-          countTasksProcessing: 0,
-          cancelledAt: '',
-          createdAt: '2020-01-01T00:00:00.000Z',
-        },
-      ],
-      meta: {
-        total: 1,
-        perPage: 10,
-        currentPage: 1,
-        nextPage: null,
-        prevPage: null,
-        firstPage: 1,
-        lastPage: 1,
-      },
-    };
+  describe('timer', () => {
+    it('should set timer', () => {
+      const spy = spyOn(
+        component.autoRefreshService,
+        'setTimer'
+      ).and.callThrough();
 
-    component.sessions = sessions;
-    fixture.detectChanges();
+      component.onTimerChange(10);
 
-    // select the first clr-dg-row
-    const row = fixture.nativeElement.querySelector('clr-dg-row');
-    // select the cell containing status
-    const cell = row.querySelectorAll('clr-dg-cell')[2];
-    // must only contains one child
-    expect(cell.childElementCount).toBe(1);
-    // child must contains the class .text-pending
-    expect(cell.children[0].classList.contains('text-danger')).toBeTruthy();
+      expect(spy).toHaveBeenCalledWith(10);
+
+      spy.calls.reset();
+    });
+
+    it('should trigger timer change', () => {
+      const child = fixture.debugElement.query(
+        By.directive(MockTimerIntervalSelectorComponent)
+      );
+
+      const spy = spyOn(component, 'onTimerChange');
+
+      const button = child.query(By.css('.timer-change'));
+      button.triggerEventHandler('click', null);
+
+      expect(component.onTimerChange).toHaveBeenCalledWith(10);
+
+      spy.calls.reset();
+    });
   });
 
-  it('should have a processing status for the first session', () => {
-    const sessions: Pagination<FormattedSession> = {
-      data: [
-        {
-          _id: '1',
-          status: 1,
-          countTasksError: 0,
-          countTasksCompleted: 0,
-          countTasksPending: 0,
-          countTasksProcessing: 1,
-          cancelledAt: '',
-          createdAt: '2020-01-01T00:00:00.000Z',
-        },
-      ],
-      meta: {
-        total: 1,
-        perPage: 10,
-        currentPage: 1,
-        nextPage: null,
-        prevPage: null,
-        firstPage: 1,
-        lastPage: 1,
-      },
-    };
+  describe('onRefreshSessions', () => {
+    it('should call getAllPaginated', () => {
+      component.onRefreshSessions({});
 
-    component.sessions = sessions;
-    fixture.detectChanges();
+      expect(http.getAllPaginated).toHaveBeenCalled();
+    });
 
-    // select the first clr-dg-row
-    const row = fixture.nativeElement.querySelector('clr-dg-row');
-    // select the cell containing status
-    const cell = row.querySelectorAll('clr-dg-cell')[2];
-    // must only contains one child
-    expect(cell.childElementCount).toBe(1);
-    // child must contains the class .text-warn
-    expect(cell.children[0].classList.contains('text-warn')).toBeTruthy();
+    it('should handle error', () => {
+      http.getAllPaginated.and.returnValue(throwError(() => 'error'));
+
+      component.onRefreshSessions({});
+
+      expect(component.errors.length).toBe(1);
+    });
+
+    it('should trigger auto refresh change', () => {
+      const child = fixture.debugElement.query(
+        By.directive(MockAutoRefreshActivatorComponent)
+      );
+
+      const spy = spyOn(component.autoRefreshService, 'toggle');
+
+      const button = child.query(By.css('.auto-refresh-change'));
+      button.triggerEventHandler('click', null);
+
+      expect(component.autoRefreshService.toggle).toHaveBeenCalled();
+
+      spy.calls.reset();
+    });
   });
 
-  it('should have a completed status for the first session', () => {
-    const sessions: Pagination<FormattedSession> = {
-      data: [
-        {
-          _id: '1',
-          status: 1,
-          countTasksError: 0,
-          countTasksCompleted: 1,
-          countTasksPending: 0,
-          countTasksProcessing: 0,
-          cancelledAt: '',
-          createdAt: '2020-01-01T00:00:00.000Z',
-        },
-      ],
-      meta: {
-        total: 1,
-        perPage: 10,
-        currentPage: 1,
-        nextPage: null,
-        prevPage: null,
-        firstPage: 1,
-        lastPage: 1,
-      },
-    };
+  describe('cancelSession', () => {
+    it('should confirm before to cancel session', () => {
+      component.sessions = sessionsPaginated;
+      fixture.detectChanges();
 
-    component.sessions = sessions;
-    fixture.detectChanges();
+      const spy = spyOn(component, 'confirmCancelSession');
 
-    // select the first clr-dg-row
-    const row = fixture.nativeElement.querySelector('clr-dg-row');
-    // select the cell containing status
-    const cell = row.querySelectorAll('clr-dg-cell')[2];
-    // must only contains one child
-    expect(cell.childElementCount).toBe(1);
-    // child must contains the class .text-success
-    expect(cell.children[0].classList.contains('text-success')).toBeTruthy();
+      const button = fixture.nativeElement.querySelector('.btn-cancel');
+      button.click();
+
+      expect(spy).toHaveBeenCalled();
+      spy.calls.reset();
+    });
+
+    it('should open confirm modal', () => {
+      component.sessions = sessionsPaginated;
+      fixture.detectChanges();
+
+      const button = fixture.nativeElement.querySelector('.btn-cancel');
+      button.click();
+      fixture.detectChanges();
+
+      const modal = fixture.nativeElement.querySelector('clr-modal');
+      expect(modal).toBeTruthy();
+    });
+
+    it('should cancel session', () => {
+      const session = {
+        _id: 'session-id',
+      } as unknown as FormattedSession;
+
+      component.cancelSession(session._id);
+
+      expect(http.cancel).toHaveBeenCalledWith(session._id);
+    });
+
+    it('should handle error', () => {
+      const session = {
+        _id: 'session-id',
+      } as unknown as FormattedSession;
+
+      http.cancel.and.returnValue(throwError(() => 'error'));
+
+      component.cancelSession(session._id);
+
+      expect(component.errors.length).toBe(1);
+    });
+
+    describe('trackSessions', () => {
+      it('should return the id of the session', () => {
+        const session: FormattedSession = {
+          _id: 'session-id',
+        } as unknown as FormattedSession;
+
+        expect(component.trackSessions(1, session)).toBe(session._id);
+      });
+    });
   });
 
-  it('should have not have any status', () => {
-    const sessions: Pagination<FormattedSession> = {
-      data: [
-        {
-          _id: '1',
-          status: 1,
-          countTasksError: 0,
-          countTasksCompleted: 0,
-          countTasksPending: 0,
-          countTasksProcessing: 0,
-          cancelledAt: '',
-          createdAt: '2020-01-01T00:00:00.000Z',
+  describe('ui', () => {
+    it('should have a error status for the first session', () => {
+      const sessions: Pagination<FormattedSession> = {
+        data: [
+          {
+            _id: '1',
+            status: 1,
+            countTasksError: 1,
+            countTasksCompleted: 1,
+            countTasksPending: 1,
+            countTasksProcessing: 1,
+            cancelledAt: '',
+            createdAt: '2020-01-01T00:00:00.000Z',
+          },
+        ],
+        meta: {
+          total: 1,
+          perPage: 10,
+          currentPage: 1,
+          nextPage: null,
+          prevPage: null,
+          firstPage: 1,
+          lastPage: 1,
         },
-      ],
-      meta: {
-        total: 1,
-        perPage: 10,
-        currentPage: 1,
-        nextPage: null,
-        prevPage: null,
-        firstPage: 1,
-        lastPage: 1,
-      },
-    };
+      };
 
-    component.sessions = sessions;
-    fixture.detectChanges();
+      component.sessions = sessions;
+      fixture.detectChanges();
 
-    // select the first clr-dg-row
-    const row = fixture.nativeElement.querySelector('clr-dg-row');
-    // select the cell containing status
-    const cell = row.querySelectorAll('clr-dg-cell')[2];
-    // must only contains one child
-    expect(cell.childElementCount).toBe(0);
+      // select the first clr-dg-row
+      const row = fixture.nativeElement.querySelector('clr-dg-row');
+      // select the cell containing status
+      const cell = row.querySelectorAll('clr-dg-cell')[2];
+      // must only contains one child
+      expect(cell.childElementCount).toBe(1);
+      // child must contains the class .text-danger
+      expect(cell.children[0].classList.contains('text-danger')).toBeTruthy();
+    });
+
+    it('should have a pending status for the first session', () => {
+      const sessions: Pagination<FormattedSession> = {
+        data: [
+          {
+            _id: '1',
+            status: 1,
+            countTasksError: 0,
+            countTasksCompleted: 0,
+            countTasksPending: 1,
+            countTasksProcessing: 0,
+            cancelledAt: '',
+            createdAt: '2020-01-01T00:00:00.000Z',
+          },
+        ],
+        meta: {
+          total: 1,
+          perPage: 10,
+          currentPage: 1,
+          nextPage: null,
+          prevPage: null,
+          firstPage: 1,
+          lastPage: 1,
+        },
+      };
+
+      component.sessions = sessions;
+      fixture.detectChanges();
+
+      // select the first clr-dg-row
+      const row = fixture.nativeElement.querySelector('clr-dg-row');
+      // select the cell containing status
+      const cell = row.querySelectorAll('clr-dg-cell')[2];
+      // must only contains one child
+      expect(cell.childElementCount).toBe(1);
+      // child must contains the class .text-pending
+      expect(cell.children[0].classList.contains('text-pending')).toBeTruthy();
+    });
+
+    it('should have a processing status for the first session', () => {
+      const sessions: Pagination<FormattedSession> = {
+        data: [
+          {
+            _id: '1',
+            status: 1,
+            countTasksError: 0,
+            countTasksCompleted: 0,
+            countTasksPending: 0,
+            countTasksProcessing: 1,
+            cancelledAt: '',
+            createdAt: '2020-01-01T00:00:00.000Z',
+          },
+        ],
+        meta: {
+          total: 1,
+          perPage: 10,
+          currentPage: 1,
+          nextPage: null,
+          prevPage: null,
+          firstPage: 1,
+          lastPage: 1,
+        },
+      };
+
+      component.sessions = sessions;
+      fixture.detectChanges();
+
+      // select the first clr-dg-row
+      const row = fixture.nativeElement.querySelector('clr-dg-row');
+      // select the cell containing status
+      const cell = row.querySelectorAll('clr-dg-cell')[2];
+      // must only contains one child
+      expect(cell.childElementCount).toBe(1);
+      // child must contains the class .text-warn
+      expect(cell.children[0].classList.contains('text-warn')).toBeTruthy();
+    });
+
+    it('should have a completed status for the first session', () => {
+      const sessions: Pagination<FormattedSession> = {
+        data: [
+          {
+            _id: '1',
+            status: 1,
+            countTasksError: 0,
+            countTasksCompleted: 1,
+            countTasksPending: 0,
+            countTasksProcessing: 0,
+            cancelledAt: '',
+            createdAt: '2020-01-01T00:00:00.000Z',
+          },
+        ],
+        meta: {
+          total: 1,
+          perPage: 10,
+          currentPage: 1,
+          nextPage: null,
+          prevPage: null,
+          firstPage: 1,
+          lastPage: 1,
+        },
+      };
+
+      component.sessions = sessions;
+      fixture.detectChanges();
+
+      // select the first clr-dg-row
+      const row = fixture.nativeElement.querySelector('clr-dg-row');
+      // select the cell containing status
+      const cell = row.querySelectorAll('clr-dg-cell')[2];
+      // must only contains one child
+      expect(cell.childElementCount).toBe(1);
+      // child must contains the class .text-success
+      expect(cell.children[0].classList.contains('text-success')).toBeTruthy();
+    });
+
+    it('should have not have any status', () => {
+      const sessions: Pagination<FormattedSession> = {
+        data: [
+          {
+            _id: '1',
+            status: 1,
+            countTasksError: 0,
+            countTasksCompleted: 0,
+            countTasksPending: 0,
+            countTasksProcessing: 0,
+            cancelledAt: '',
+            createdAt: '2020-01-01T00:00:00.000Z',
+          },
+        ],
+        meta: {
+          total: 1,
+          perPage: 10,
+          currentPage: 1,
+          nextPage: null,
+          prevPage: null,
+          firstPage: 1,
+          lastPage: 1,
+        },
+      };
+
+      component.sessions = sessions;
+      fixture.detectChanges();
+
+      // select the first clr-dg-row
+      const row = fixture.nativeElement.querySelector('clr-dg-row');
+      // select the cell containing status
+      const cell = row.querySelectorAll('clr-dg-cell')[2];
+      // must only contains one child
+      expect(cell.childElementCount).toBe(0);
+    });
   });
 });
