70e9046b575ae1f14fc050d815d16e7d861edd16
Estéban Soubiran <esoubiran@aneo.fr>
2022-08-09 12:32:52 +0200

    feat: add browser title

--- apps/app/src/app/pages/pages/how-to-use/how-to-use.component.ts
+++ apps/app/src/app/pages/pages/how-to-use/how-to-use.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BrowserTitleService, LanguageService } from '../../../core';
 
 @Component({
   selector: 'app-pages-how-to-use',
   templateUrl: './how-to-use.component.html',
   styleUrls: ['./how-to-use.component.scss'],
 })
-export class HowToUseComponent {}
+export class HowToUseComponent implements OnInit {
+  constructor(
+    private browserTitleService: BrowserTitleService,
+    private languageService: LanguageService
+  ) {}
+
+  ngOnInit(): void {
+    this.browserTitleService.setTitle(
+      this.languageService.instant('navigation.how_to_use')
+    );
+  }
+}
