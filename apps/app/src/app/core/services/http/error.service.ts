bc560c5d3c7cdd6776c79221743ad6b72420fe84
Estéban Soubiran <esoubiran@aneo.fr>
2022-08-11 10:34:29 +0200

    fix: update logger from files

--- apps/app/src/app/core/services/http/error.service.ts
+++ apps/app/src/app/core/services/http/error.service.ts
@@ -2,12 +2,9 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { Logger } from '@nestjs/common';
 
 @Injectable()
 export class ErrorService {
-  private readonly logger = new Logger(ErrorService.name);
-
   constructor(private router: Router) {}
 
   handleError(
@@ -16,7 +13,7 @@ export class ErrorService {
   ): Observable<null> {
     switch (errorResponse.status) {
       default: {
-        this.logger.error(errorResponse);
+        console.error(errorResponse);
         this.router.navigate(['/', 'error']);
         return of(null);
       }
