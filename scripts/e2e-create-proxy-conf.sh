94ca61694713eb20bdf054b42efa0e2d633c82c2
Estéban Soubiran <esoubiran@aneo.fr>
2022-10-18 11:40:38 +0200

    chore: format

--- scripts/e2e-create-proxy-conf.sh
+++ scripts/e2e-create-proxy-conf.sh
@@ -9,12 +9,10 @@ if [ "$1" == "-h" -o  "$1" == "--help" ]; then
     exit 1
 fi
 
-dir=$(dirname $0)
-
 # create proxy.conf.json
 echo "{
   \"/api\": {
     \"target\": \"http://localhost:3333\",
     \"secure\": false
   }
-}" > $dir/../apps/app-e2e/proxy.conf.json
+}" > ./apps/app-e2e/proxy.conf.json
