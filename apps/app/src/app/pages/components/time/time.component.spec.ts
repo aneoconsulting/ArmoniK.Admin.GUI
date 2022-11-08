04bff14498ad6bc5ee731433d955cc992b8f811e
Estéban Soubiran <esoubiran@aneo.fr>
2022-11-08 15:56:45 +0100

    test: add for timer

---
+++ apps/app/src/app/pages/components/time/time.component.spec.ts
@@ -0,0 +1,48 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { first, take } from 'rxjs';
+import { TimeComponent } from './time.component';
+
+describe('TimeComponent', () => {
+  let component: TimeComponent;
+  let fixture: ComponentFixture<TimeComponent>;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [TimeComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TimeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('time$', () => {
+    it('should return a new date on subscription', (done) => {
+      component.time$.pipe(first()).subscribe((date) => {
+        expect(date).toBeInstanceOf(Date);
+        done();
+      });
+    });
+
+    it('should return a new date every second', (done) => {
+      let firstDate: Date;
+      let secondDate: Date;
+
+      component.time$.pipe(take(2)).subscribe((date) => {
+        if (!firstDate) {
+          firstDate = date;
+        } else {
+          secondDate = date;
+          expect(secondDate).not.toEqual(firstDate);
+          done();
+        }
+      });
+    });
+  });
+});
