963f8ddad04865b55b6544de94fb5bc5f3bcf736
Estéban Soubiran <esoubiran@aneo.fr>
2022-11-14 11:43:04 +0100

    fix: remove generated files

--- apps/app/src/app/core/types/proto/applications-service.pb.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-// @ts-nocheck
-//
-// THIS IS A GENERATED FILE
-// DO NOT MODIFY IT! YOUR CHANGES WILL BE LOST
-import * as googleProtobuf000 from '@ngx-grpc/well-known-types';
-import * as armonikApiGrpcV1001 from './objects.pb';
-import * as armonikApiGrpcV1Applications002 from './applications-common.pb';
