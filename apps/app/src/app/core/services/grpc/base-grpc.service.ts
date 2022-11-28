0e10f93d79f8f471afb2c233a986e3ed9ea847eb
Estéban <103997512+esoubiran-aneo@users.noreply.github.com>
2022-11-28 12:04:51 +0100

    feat: fetch tasks using grpc (#286)
    * feat: fetch tasks using grpc
    
    * chore: format
    
    * fix: keep types folder using gitkeep
    
    * feat: add to query on click
    
    * fix: update proto generation command
    
    * feat: update api
    
    * feat: cancel tasks
    
    * feat: add sorting
    
    * feat: add tab title
    
    * feat: update status in template
    
    * fix: filters
    
    * ci: fix build
    
    * chore: format
    
    * fix: comments
    
    * fix: timeout
    
    * fix: timeout in sessions
    
    * fix: seq up
    
    * fix: comments

---
+++ apps/app/src/app/core/services/grpc/base-grpc.service.ts
@@ -0,0 +1,8 @@
+import { mergeMap, throwError, timer } from 'rxjs';
+
+export class BaseGrpcService {
+  // Timeout for gRPC requests. 8 seconds is an arbitrary value that should be under 10 seconds, which is the default value for auto refresh.
+  protected _timeout$ = timer(8_000).pipe(
+    mergeMap(() => throwError(() => new Error('gRPC Timeout')))
+  );
+}
