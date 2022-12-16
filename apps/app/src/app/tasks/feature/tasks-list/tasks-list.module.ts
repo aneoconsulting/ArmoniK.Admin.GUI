5d1d8b67018263b80befe40897ec604f31faa4f1
Faust1-2was-aneo <fdewas@aneo.fr>
2022-12-16 16:57:59 +0100

    added id-filters to tasks page

--- apps/app/src/app/tasks/feature/tasks-list/tasks-list.module.ts
+++ apps/app/src/app/tasks/feature/tasks-list/tasks-list.module.ts
@@ -21,6 +21,7 @@ import { TranslateModule } from '@ngx-translate/core';
 import {
   TaskStatusFilterComponent,
   DateFilterComponent,
+  IdFilterComponent,
 } from '../../../shared/feature/filters';
 import { TasksListRoutingModule } from './tasks-list-routing.module';
 import { TasksListComponent } from './tasks-list.page';
@@ -40,6 +41,7 @@ import { TasksListComponent } from './tasks-list.page';
     TranslateModule,
     TasksListRoutingModule,
     TaskStatusFilterComponent,
+    IdFilterComponent,
     DateFilterComponent,
     NgClass,
     AsyncPipe,
