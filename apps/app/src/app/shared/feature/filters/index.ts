7b594fff61368acbd52aa59572b0ae0b5d38d879
Faust1 <117363666+Faust1-2was-Aneo@users.noreply.github.com>
2022-12-16 18:43:45 +0100

    feat: create combobox filter (#339)

--- apps/app/src/app/shared/feature/filters/index.ts
+++ apps/app/src/app/shared/feature/filters/index.ts
@@ -1,2 +1,3 @@
+export { ComboBoxFilterComponent } from './combobox-filter/combobox-filter.component';
 export { IdFilterComponent } from './id-filter/id-filter.component';
 export { DateFilterComponent } from './date-filter/date-filter.component';
