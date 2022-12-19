39f436b1facd1acb435a698cf93d5f94fffbc673
Faust1 <117363666+Faust1-2was-Aneo@users.noreply.github.com>
2022-12-19 09:17:07 +0100

    feat: create a select filter (#338)

--- apps/app/src/app/shared/feature/filters/index.ts
+++ apps/app/src/app/shared/feature/filters/index.ts
@@ -1,3 +1,4 @@
+export { SelectFilterComponent } from './select-filter/select-filter.component';
 export { ComboBoxFilterComponent } from './combobox-filter/combobox-filter.component';
 export { IdFilterComponent } from './id-filter/id-filter.component';
 export { DateFilterComponent } from './date-filter/date-filter.component';
