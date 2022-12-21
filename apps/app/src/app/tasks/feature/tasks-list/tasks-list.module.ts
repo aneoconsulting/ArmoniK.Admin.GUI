5b30ba8fb1a8cdd000a485b3fcac0c364a4541ea
Faust1-2was-aneo <fdewas@aneo.fr>
2022-12-21 11:05:01 +0100

    added clear-order to tasks-page

--- apps/app/src/app/tasks/feature/tasks-list/tasks-list.module.ts
+++ apps/app/src/app/tasks/feature/tasks-list/tasks-list.module.ts
@@ -18,6 +18,7 @@ import {
   ClrModalModule,
 } from '@clr/angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { ClearOrderComponent } from '../../../shared/feature';
 import {
   SelectFilterComponent,
   DateFilterComponent,
@@ -43,6 +44,7 @@ import { TasksListComponent } from './tasks-list.page';
     SelectFilterComponent,
     IdFilterComponent,
     DateFilterComponent,
+    ClearOrderComponent,
     NgClass,
     AsyncPipe,
     DatePipe,
