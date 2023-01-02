42554414fcb5edb713f89c3ecaa306197e34d9e5
Estéban Soubiran <esoubiran@aneo.fr>
2023-01-02 10:30:39 +0100

    feat: add translations for status

--- apps/app/src/app/sessions/feature/sessions-list/sessions-list.page.ts
+++ apps/app/src/app/sessions/feature/sessions-list/sessions-list.page.ts
@@ -107,6 +107,19 @@ export class SessionsListComponent implements OnInit {
     return SessionStatus;
   }
 
+  public getStatusLabel(status: number): string {
+    switch (status) {
+      case SessionStatus.SESSION_STATUS_CANCELLED:
+        return $localize`Cancelled`;
+      case SessionStatus.SESSION_STATUS_RUNNING:
+        return $localize`Running`;
+      case SessionStatus.SESSION_STATUS_UNSPECIFIED:
+        return $localize`Unspecified`;
+      default:
+        return $localize`Unknown`;
+    }
+  }
+
   public onUpdateInterval(value: number) {
     this._intervalValue.next(value);
 
