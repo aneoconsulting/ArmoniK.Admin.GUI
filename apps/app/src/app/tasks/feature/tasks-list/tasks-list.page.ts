f13621ecc3921ce3f4f731d4307a27bb721e5efc
Faust1-2was-aneo <fdewas@aneo.fr>
2023-02-02 15:39:42 +0100

    removed task id filter

--- apps/app/src/app/tasks/feature/tasks-list/tasks-list.page.ts
+++ apps/app/src/app/tasks/feature/tasks-list/tasks-list.page.ts
@@ -105,11 +105,6 @@ export class TasksListComponent implements OnInit {
    * Filters observables.
    * We are not using the queryParam functions because they are called in a infinite loop with the async pipe.
    */
-  filterTaskId: string | null = this._settingsService.queryParam(
-    this._activatedRoute.snapshot.queryParams,
-    'taskId'
-  );
-
   filterSessionId: string | null = this._settingsService.queryParam(
     this._activatedRoute.snapshot.queryParams,
     'sessionId'
