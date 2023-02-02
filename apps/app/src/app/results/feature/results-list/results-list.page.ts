6696f785f9ff0d76fabd445be274b00a73ada60f
Faust1-2was-aneo <fdewas@aneo.fr>
2023-02-02 15:05:45 +0100

    statuslist is now localized

--- apps/app/src/app/results/feature/results-list/results-list.page.ts
+++ apps/app/src/app/results/feature/results-list/results-list.page.ts
@@ -126,7 +126,9 @@ export class ResultsListComponent implements OnInit {
         .filter((key) => !Number.isInteger(parseInt(key)))
         .map((key) => ({
           value: ResultStatus[key as keyof typeof ResultStatus] as number,
-          label: key,
+          label: this.getStatusLabel(
+            ResultStatus[key as keyof typeof ResultStatus]
+          ),
         })),
     ];
   }
