1b10a7173c69e630d0ba5e9c945c5094a8e8cc32
esoubiran-aneo <esoubiran@aneo.fr>
2023-03-15 16:46:11 +0100

    test: fix dashboard

---
+++ libs/pages/dashboard/src/lib/feature/page-dashboard-feature/pages-dashboard-feature.component.spec.ts
@@ -0,0 +1,31 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { GrpcCoreModule } from '@ngx-grpc/core';
+import { GrpcWebClientModule } from '@ngx-grpc/grpc-web-client';
+import { DashboardComponent } from './page-dashboard-feature.component';
+
+describe('PagesDashboardFeatureComponent', () => {
+  let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
+
+  beforeEach((() => {
+    TestBed.configureTestingModule({
+      imports: [DashboardComponent,
+        GrpcCoreModule.forRoot(),
+        GrpcWebClientModule.forRoot({
+          settings: {
+            host: '',
+          },
+        }),],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- libs/pages/dashboard/src/test.ts
+++ libs/pages/dashboard/src/test.ts
@@ -1,6 +1,7 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/dist/zone';
+import '@angular/localize/init';
 
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
