4ec92db1a76bc696db907ebd335887c579a7eade
esoubiran-aneo <esoubiran@aneo.fr>
2023-03-15 17:07:39 +0100

    chore: format

--- libs/pages/dashboard/src/lib/feature/page-dashboard-feature/pages-dashboard-feature.component.spec.ts
+++ libs/pages/dashboard/src/lib/feature/page-dashboard-feature/pages-dashboard-feature.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { GrpcCoreModule } from '@ngx-grpc/core';
 import { GrpcWebClientModule } from '@ngx-grpc/grpc-web-client';
 import { DashboardComponent } from './page-dashboard-feature.component';
@@ -7,17 +7,19 @@ describe('PagesDashboardFeatureComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
-  beforeEach((() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [DashboardComponent,
+      imports: [
+        DashboardComponent,
         GrpcCoreModule.forRoot(),
         GrpcWebClientModule.forRoot({
           settings: {
             host: '',
           },
-        }),],
+        }),
+      ],
     }).compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
