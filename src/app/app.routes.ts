0a79a2a6c797aad3a090afeee9421c081c30c492
esoubiran-aneo <esoubiran@aneo.fr>
2023-05-23 18:26:01 +0200

    fix: redirect to dashboard by default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
     path: 'profile',
     loadChildren: () => import('./profile/routes').then(mod => mod.PROFILE_ROUTES)
   },
