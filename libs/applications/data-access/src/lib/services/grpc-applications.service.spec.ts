bb609cb598c8329663b6c5ab97e245016d693376
esoubiran-aneo <esoubiran@aneo.fr>
2023-08-25 13:45:49 +0200

    chore: format

--- libs/applications/data-access/src/lib/services/grpc-applications.service.spec.ts
+++ libs/applications/data-access/src/lib/services/grpc-applications.service.spec.ts
@@ -40,150 +40,150 @@ describe('GrpcApplicationsService', () => {
     expect(service).toBeTruthy();
   });
 
-//   it('should create a default list of request params', () => {
-//     const state: ClrDatagridStateInterface = {};
-//     const requestParams = service.createListRequestParams(state);
-//     expect(requestParams).toEqual({
-//       page: 0,
-//       pageSize: 10,
-//       orderBy: [ApplicationRawField.APPLICATION_RAW_FIELD_NAME],
-//       order: 1,
-//       filter: {} as ListApplicationsRequest.Filter,
-//     });
-//   });
+  //   it('should create a default list of request params', () => {
+  //     const state: ClrDatagridStateInterface = {};
+  //     const requestParams = service.createListRequestParams(state);
+  //     expect(requestParams).toEqual({
+  //       page: 0,
+  //       pageSize: 10,
+  //       orderBy: [ApplicationRawField.APPLICATION_RAW_FIELD_NAME],
+  //       order: 1,
+  //       filter: {} as ListApplicationsRequest.Filter,
+  //     });
+  //   });
 
-//   it('should create a list of specified request params', () => {
-//     const state: ClrDatagridStateInterface = {
-//       page: {
-//         current: 2,
-//         size: 50,
-//       },
-//       sort: {
-//         by: ApplicationRawField.APPLICATION_RAW_FIELD_NAMESPACE as unknown as ClrDatagridComparatorInterface<number>,
-//         reverse: true,
-//       },
-//       filters: [
-//         {
-//           property: 'name',
-//           value: 'Some test name',
-//         },
-//       ],
-//     };
-//     const requestParams = service.createListRequestParams(state);
-//     expect(requestParams).toEqual({
-//       page: 1,
-//       pageSize: 50,
-//       orderBy: [ApplicationRawField.APPLICATION_RAW_FIELD_NAMESPACE],
-//       order: 2,
-//       filter: {
-//         name: 'Some test name',
-//       } as ListApplicationsRequest.Filter,
-//     });
-//   });
+  //   it('should create a list of specified request params', () => {
+  //     const state: ClrDatagridStateInterface = {
+  //       page: {
+  //         current: 2,
+  //         size: 50,
+  //       },
+  //       sort: {
+  //         by: ApplicationRawField.APPLICATION_RAW_FIELD_NAMESPACE as unknown as ClrDatagridComparatorInterface<number>,
+  //         reverse: true,
+  //       },
+  //       filters: [
+  //         {
+  //           property: 'name',
+  //           value: 'Some test name',
+  //         },
+  //       ],
+  //     };
+  //     const requestParams = service.createListRequestParams(state);
+  //     expect(requestParams).toEqual({
+  //       page: 1,
+  //       pageSize: 50,
+  //       orderBy: [ApplicationRawField.APPLICATION_RAW_FIELD_NAMESPACE],
+  //       order: 2,
+  //       filter: {
+  //         name: 'Some test name',
+  //       } as ListApplicationsRequest.Filter,
+  //     });
+  //   });
 
-//   it('should create a default request query', () => {
-//     const result = service.createListRequestQueryParams(
-//       {
-//         page: 0,
-//         pageSize: 10,
-//         orderBy: [ApplicationRawField.APPLICATION_RAW_FIELD_NAME],
-//         order: SortDirection.SORT_DIRECTION_ASC,
-//         filter: {} as ListApplicationsRequest.Filter,
-//       },
-//       10000
-//     );
-//     expect(result).toEqual({
-//       page: undefined,
-//       pageSize: undefined,
-//       interval: undefined,
-//       orderBy: undefined,
-//       order: undefined,
-//     });
-//   });
+  //   it('should create a default request query', () => {
+  //     const result = service.createListRequestQueryParams(
+  //       {
+  //         page: 0,
+  //         pageSize: 10,
+  //         orderBy: [ApplicationRawField.APPLICATION_RAW_FIELD_NAME],
+  //         order: SortDirection.SORT_DIRECTION_ASC,
+  //         filter: {} as ListApplicationsRequest.Filter,
+  //       },
+  //       10000
+  //     );
+  //     expect(result).toEqual({
+  //       page: undefined,
+  //       pageSize: undefined,
+  //       interval: undefined,
+  //       orderBy: undefined,
+  //       order: undefined,
+  //     });
+  //   });
 
-//   it('should create a request query', () => {
-//     const result = service.createListRequestQueryParams(
-//       {
-//         page: 2,
-//         pageSize: 50,
-//         orderBy: [ApplicationRawField.APPLICATION_RAW_FIELD_NAMESPACE],
-//         order: SortDirection.SORT_DIRECTION_DESC,
-//         filter: {
-//           name: 'Some test name',
-//         } as ListApplicationsRequest.Filter,
-//       },
-//       30000
-//     );
-//     expect(result).toEqual({
-//       page: 2,
-//       pageSize: 50,
-//       interval: 30000,
-//       orderBy: [3],
-//       order: 2,
-//       name: 'Some test name',
-//     });
-//   });
+  //   it('should create a request query', () => {
+  //     const result = service.createListRequestQueryParams(
+  //       {
+  //         page: 2,
+  //         pageSize: 50,
+  //         orderBy: [ApplicationRawField.APPLICATION_RAW_FIELD_NAMESPACE],
+  //         order: SortDirection.SORT_DIRECTION_DESC,
+  //         filter: {
+  //           name: 'Some test name',
+  //         } as ListApplicationsRequest.Filter,
+  //       },
+  //       30000
+  //     );
+  //     expect(result).toEqual({
+  //       page: 2,
+  //       pageSize: 50,
+  //       interval: 30000,
+  //       orderBy: [3],
+  //       order: 2,
+  //       name: 'Some test name',
+  //     });
+  //   });
 
-//   it('should create a default list of request options', () => {
-//     const result = service.createListRequestOptions({
-//       page: 0,
-//       pageSize: 10,
-//       orderBy: [ApplicationRawField.APPLICATION_RAW_FIELD_NAME],
-//       order: SortDirection.SORT_DIRECTION_ASC,
-//       filter: {} as ListApplicationsRequest.Filter,
-//     });
-//     expect(result).toEqual(
-//       new ListApplicationsRequest({
-//         page: 0,
-//         pageSize: 10,
-//         sort: {
-//           fields: [
-//             {
-//               applicationField: ApplicationRawField.APPLICATION_RAW_FIELD_NAME,
-//             },
-//           ],
-//           direction: SortDirection.SORT_DIRECTION_ASC,
-//         },
-//         filter: {
-//           name: '',
-//           namespace: '',
-//           version: '',
-//           service: '',
-//         },
-//       })
-//     );
-//   });
+  //   it('should create a default list of request options', () => {
+  //     const result = service.createListRequestOptions({
+  //       page: 0,
+  //       pageSize: 10,
+  //       orderBy: [ApplicationRawField.APPLICATION_RAW_FIELD_NAME],
+  //       order: SortDirection.SORT_DIRECTION_ASC,
+  //       filter: {} as ListApplicationsRequest.Filter,
+  //     });
+  //     expect(result).toEqual(
+  //       new ListApplicationsRequest({
+  //         page: 0,
+  //         pageSize: 10,
+  //         sort: {
+  //           fields: [
+  //             {
+  //               applicationField: ApplicationRawField.APPLICATION_RAW_FIELD_NAME,
+  //             },
+  //           ],
+  //           direction: SortDirection.SORT_DIRECTION_ASC,
+  //         },
+  //         filter: {
+  //           name: '',
+  //           namespace: '',
+  //           version: '',
+  //           service: '',
+  //         },
+  //       })
+  //     );
+  //   });
 
-//   it('should create a list of request options', () => {
-//     const result = service.createListRequestOptions({
-//       page: 2,
-//       pageSize: 50,
-//       orderBy: [ApplicationRawField.APPLICATION_RAW_FIELD_NAMESPACE],
-//       order: SortDirection.SORT_DIRECTION_DESC,
-//       filter: {
-//         name: 'Some test name',
-//       } as ListApplicationsRequest.Filter,
-//     });
-//     expect(result).toEqual(
-//       new ListApplicationsRequest({
-//         page: 2,
-//         pageSize: 50,
-//         sort: {
-//           fields: [
-//             {
-//               applicationField:
-//                 ApplicationRawField.APPLICATION_RAW_FIELD_NAMESPACE,
-//             },
-//           ],
-//           direction: SortDirection.SORT_DIRECTION_DESC,
-//         },
-//         filter: {
-//           name: 'Some test name',
-//           namespace: '',
-//           version: '',
-//           service: '',
-//         },
-//       })
-//     );
-//   });
+  //   it('should create a list of request options', () => {
+  //     const result = service.createListRequestOptions({
+  //       page: 2,
+  //       pageSize: 50,
+  //       orderBy: [ApplicationRawField.APPLICATION_RAW_FIELD_NAMESPACE],
+  //       order: SortDirection.SORT_DIRECTION_DESC,
+  //       filter: {
+  //         name: 'Some test name',
+  //       } as ListApplicationsRequest.Filter,
+  //     });
+  //     expect(result).toEqual(
+  //       new ListApplicationsRequest({
+  //         page: 2,
+  //         pageSize: 50,
+  //         sort: {
+  //           fields: [
+  //             {
+  //               applicationField:
+  //                 ApplicationRawField.APPLICATION_RAW_FIELD_NAMESPACE,
+  //             },
+  //           ],
+  //           direction: SortDirection.SORT_DIRECTION_DESC,
+  //         },
+  //         filter: {
+  //           name: 'Some test name',
+  //           namespace: '',
+  //           version: '',
+  //           service: '',
+  //         },
+  //       })
+  //     );
+  //   });
 });
