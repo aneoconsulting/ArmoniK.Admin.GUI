e8764241407111d9307c6ad0c078cffc8786d191
yannick-aneo <136307285+yannick-aneo@users.noreply.github.com>
2023-08-30 10:43:42 +0200

    feat: edition in reorganize-line dialog (#552)
    Co-authored-by: Estéban <103997512+esoubiran-aneo@users.noreply.github.com>
    Co-authored-by: esoubiran-aneo <esoubiran@aneo.fr>

--- src/app/dashboard/components/reorganize-lines-dialog.component.ts
+++ src/app/dashboard/components/reorganize-lines-dialog.component.ts
@@ -1,11 +1,14 @@
 import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
 import { NgFor } from '@angular/common';
-import { Component, Inject, OnInit, inject } from '@angular/core';
+import { Component, EventEmitter, Inject, OnInit, Output, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import { ReorganizeLinesDialogData, ReorganizeLinesDialogResult } from '@app/types/dialog';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { EditNameLineData, EditNameLineResult, ReorganizeLinesDialogData, ReorganizeLinesDialogResult } from '@app/types/dialog';
 import { IconsService } from '@services/icons.service';
+import { EditNameLineDialogComponent } from './edit-name-line-dialog.component';
 import { Line } from '../types';
 
 @Component({
@@ -17,11 +20,19 @@ import { Line } from '../types';
   <p i18n="Dialog description">Drag and drop lines to update the order</p>
 
   <div class="lines" cdkDropList (cdkDropListDropped)="drop($event)">
-    <div class="line" *ngFor="let line of lines" cdkDrag>
-      <mat-icon mat-icon aria-hidden="true" i18n-aria-label aria-label="Drag status" [fontIcon]="getIcon('drag')"></mat-icon>
-      <span class="line-name">{{ line.name }}</span>
-      <!-- TODO: Add a button to rename the line (use same dialog as the one used for a line "Edit Name Line") -->
-      <!-- TODO: Add a button to remove the line -->
+    <div class="line" *ngFor="let line of lines; let index = index" cdkDrag>
+      <div class="line-name">
+        <mat-icon mat-icon aria-hidden="true" i18n-aria-label aria-label="Drag status" [fontIcon]="getIcon('drag')"></mat-icon>
+        <span class="line-name">{{ line.name }}</span>
+      </div>
+      <div clas="line-actions">
+        <button mat-icon-button (click)="onEditNameLine(line, index)" i18n-matTooltip matTooltip="Edit Line">
+          <mat-icon aria-hidden="true"  [fontIcon]="getIcon('edit')"></mat-icon>
+        </button>
+        <button mat-icon-button (click)="onDeleteLine(line)" i18n-matTooltip matTooltip="Delete Line">
+          <mat-icon aria-hidden="true" [fontIcon]="getIcon('delete')"></mat-icon>
+        </button>
+      </div>
     </div>
   </div>
 </mat-dialog-content>
@@ -39,11 +50,23 @@ import { Line } from '../types';
 
 .line {
   display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  cursor: move;
+}
+
+.line-name {
+  display: flex;
   align-items: center;
   gap: 0.5rem;
   padding: 0.5rem;
+}
 
-  cursor: move;
+.line-actions {
+  display: flex;
+  flex-direction: row;
+  gap: 0.5rem;
 }
 
 .cdk-drag-preview {
@@ -72,11 +95,18 @@ import { Line } from '../types';
     MatIconModule,
     MatButtonModule,
     DragDropModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatTooltipModule,
   ]
 })
 export class ReorganizeLinesDialogComponent implements OnInit {
+  @Output() lineChange: EventEmitter<void> = new EventEmitter<void>();
+  @Output() lineDelete: EventEmitter<Line> = new EventEmitter<Line>();
+
   readonly #dialogRef = inject(MatDialogRef<ReorganizeLinesDialogData, ReorganizeLinesDialogResult>);
   readonly #iconsService = inject(IconsService);
+  readonly #dialog = inject(MatDialog);
 
   lines: Line[] = [];
 
@@ -100,6 +130,32 @@ export class ReorganizeLinesDialogComponent implements OnInit {
     moveItemInArray(this.lines, event.previousIndex, event.currentIndex);
   }
 
+  onDeleteLine( line: Line) {
+    const index = this.lines.indexOf(line);
+    if (index > -1) {
+      this.lines.splice(index, 1);
+    }
+  }
+
+  onEditNameLine(line: Line, index: number) {
+    const dialogRef: MatDialogRef<EditNameLineDialogComponent, EditNameLineResult> = this.#dialog.open<EditNameLineDialogComponent, EditNameLineData, EditNameLineResult>(EditNameLineDialogComponent, {
+      data: {
+        name: line.name
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (!result) return;
+      const selectedLine = this.lines[index];
+      const changeSelectedNameLine = (line: Line, newName: string): void => {
+        if(line.name === newName) {
+          line.name = result.name;
+        }
+      };
+      this.lines.map(line => changeSelectedNameLine(line, selectedLine.name));
+    });
+
+  }
   trackByLine(index: number, line: Line): string {
     return line.name + index;
   }
--- src/app/dashboard/index.component.ts
+++ src/app/dashboard/index.component.ts
@@ -82,7 +82,7 @@ import { Line } from './types';
     <app-page-section-header icon="adjust">
       <span i18n="Section title">{{ line.name }}</span>
     </app-page-section-header>
-    <app-dashboard-line [line]="line"  (lineChange)="onSaveChange()" (lineDelete)="onDeleteLine($event)"></app-dashboard-line>
+    <app-dashboard-line [line]="line" (lineChange)="onSaveChange()" (lineDelete)="onDeleteLine($event)"></app-dashboard-line>
   </app-page-section>
 </main>
   `,
