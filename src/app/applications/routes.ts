2914f30c01deb0c0d4a4fd3791b5feb4577cd3ec
Faust1 <fdewas@aneo.fr>
2023-09-12 16:28:04 +0200

    last update

--- src/app/applications/routes.ts
+++ src/app/applications/routes.ts
@@ -1,13 +1,6 @@
-import { NgModule } from '@angular/core';
-import { Route, RouterModule } from '@angular/router';
+import { Route } from '@angular/router';
 import { IndexComponent } from './index.component';
 
 export const APPLICATIONS_ROUTES: Route[] = [
   { path: '', component: IndexComponent },
 ];
-
-@NgModule({
-  imports: [RouterModule.forChild(APPLICATIONS_ROUTES)],
-  exports: [RouterModule]
-})
-export class ApplicationRouterModule {}
