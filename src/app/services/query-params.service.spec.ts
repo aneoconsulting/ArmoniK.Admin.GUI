73b8e22f5732748d68e855b466e3029a673fc3ec
Faust1 <fdewas@aneo.fr>
2023-09-13 16:03:32 +0200

    Created tests for query-params services

---
+++ src/app/services/query-params.service.spec.ts
@@ -0,0 +1,74 @@
+import { ApplicationRaw } from '@aneoconsultingfr/armonik.api.angular';
+import { TestBed } from '@angular/core/testing';
+import { FiltersOr } from '@app/types/filters';
+import { ListOptions } from '@app/types/options';
+import { QueryParamsService } from './query-params.service';
+
+describe('QueryParamsService', () => {
+  
+  let service: QueryParamsService;
+
+  beforeEach(() => {
+    service = TestBed.configureTestingModule({
+      providers: [QueryParamsService]
+    }).inject(QueryParamsService);
+  });
+
+  it('Should run', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('Should create options', () => {
+    const options: ListOptions<ApplicationRaw> = {
+      pageIndex: 1,
+      pageSize: 10,
+      sort: {
+        active: ['name','service'] as unknown as keyof ApplicationRaw,
+        direction: 'asc'
+      }
+    };
+    const optionsResult = {
+      pageIndex:'1',
+      pageSize:'10',
+      sortField:'name,service',
+      sortDirection:'asc'
+    };
+    expect(service.createOptions(options)).toEqual(optionsResult);
+  });
+
+  it('Should create Filters', () => {
+    const filters: FiltersOr<number, number> = [
+      [{
+        for: 'root',
+        field: 2,
+        value: 'filterValue',
+        operator: 1
+      },
+      {
+        for: 'root',
+        field: 1,
+        value: 1,
+        operator: 0
+      }],
+      [{
+        for: 'options',
+        field: 4,
+        value: 42,
+        operator: 3
+      },
+      {
+        for: 'root',
+        field: 3,
+        value: 'myField',
+        operator: 2
+      }]
+    ];
+    const filtersResult = {
+      '0-root-2-1': 'filterValue',
+      '0-root-1-0': 1,
+      '1-options-4-3': 42,
+      '1-root-3-2': 'myField'
+    };
+    expect(service.createFilters(filters)).toEqual(filtersResult);
+  });
+});
