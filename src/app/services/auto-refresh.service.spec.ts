1f7ccfcdcc68b593f12f079a59a1fa4a1db68ac3
Faust1 <fdewas@aneo.fr>
2023-09-13 11:45:48 +0200

    added all tests for auto-refresh

--- src/app/services/auto-refresh.service.spec.ts
+++ src/app/services/auto-refresh.service.spec.ts
@@ -30,7 +30,7 @@ describe('Auto-refresh service', () => {
   });
 
   describe('createInterval', () => {
-    it('Should return an interval when one is gived', () => {
+    it('Should return an interval when a value is given', () => {
       const intervalSubjet: BehaviorSubject<number> = new BehaviorSubject(1);
       const stopIntervalSubject: Subject<void> = new Subject();
 
@@ -38,11 +38,29 @@ describe('Auto-refresh service', () => {
         .createInterval(intervalSubjet, stopIntervalSubject)
         .subscribe(interval => expect(interval).toEqual(1000));
     });
+    it('Should return an interval when 0 is given', () => {
+      const intervalSubjet: BehaviorSubject<number> = new BehaviorSubject(0);
+      const stopIntervalSubject: Subject<void> = new Subject();
+
+      service
+        .createInterval(intervalSubjet, stopIntervalSubject)
+        .subscribe(interval => expect(interval).toEqual(0));
+    });
     it('Should not return anything when no value is given', () => {
       const intervalSubjet: Subject<number> = new Subject();
       const stopIntervalSubject: Subject<void> = new Subject();
 
-      expect(service.createInterval(intervalSubjet, stopIntervalSubject)).toBeUndefined();
+      service
+        .createInterval(intervalSubjet, stopIntervalSubject)
+        .subscribe(value => expect(value).toBeUndefined);
+    });
+    it('Should not return anything when a negative value is given', () => {
+      const intervalSubjet: BehaviorSubject<number> = new BehaviorSubject(1);
+      const stopIntervalSubject: Subject<void> = new Subject();
+
+      service
+        .createInterval(intervalSubjet, stopIntervalSubject)
+        .subscribe(value => expect(value).toBeUndefined);
     });
   });
 });
--- src/app/services/auto-refresh.service.ts
+++ src/app/services/auto-refresh.service.ts
@@ -1,26 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Subject, filter, interval, merge, switchMap, takeUntil } from 'rxjs';
+import { Subject, interval, merge, of, switchMap, takeUntil } from 'rxjs';
 
 @Injectable()
 export class AutoRefreshService {
   createInterval(intervalSubject: Subject<number>, stopIntervalSubject: Subject<void>) {
     return merge(intervalSubject).pipe(
-      filter((value) => {
-        if (!value) {
-          return false;
-        }
-        // Interval can be 0 but we don't want to start the timer
-        return true;
-      }),
       switchMap((value) => {
-        // Value is in seconds, we need to convert it to milliseconds
-        return interval((value as number) * 1000).pipe(takeUntil(stopIntervalSubject));
+        if (!value || value < 0) return of(undefined);
+        else return interval((value as number) * 1000).pipe(takeUntil(stopIntervalSubject));
       })
     );
   }
 
   autoRefreshTooltip(interval: number): string {
-    if (interval === 0) {
+    if (interval <= 0) {
       return 'Auto-refresh is disabled';
     }
 
