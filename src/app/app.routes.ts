7e26d8c7d6762eb0c88780054a2ee989d28656b6
Faust1 <117363666+Faust1-2was-Aneo@users.noreply.github.com>
2023-09-13 10:17:50 +0200

    chore: update lauching page to dashboard (#586)
    Co-authored-by: yannick-aneo <136307285+yannick-aneo@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'partitions',
+    redirectTo: 'dashboard',
     pathMatch: 'full',
   },
   {
