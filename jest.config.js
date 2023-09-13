b29d613f91bbd778051c7f379bd700f488a2091c
Faust1 <117363666+Faust1-2was-Aneo@users.noreply.github.com>
2023-09-13 11:24:20 +0200

    chore: use jest (#584)
    Co-authored-by: yannick-aneo <136307285+yannick-aneo@users.noreply.github.com>

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/* eslint-env es6 */
+/* eslint-disable no-console */
+const { TextEncoder, TextDecoder } = require('util')
+
+module.exports = {
+  globals: {
+    TextDecoder: TextDecoder,
+    TextEncoder: TextEncoder
+  }
+}
