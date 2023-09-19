807874213855f3c483b087e6de2a57faf4574362
Yannick BLEY (ANEO) <ybley@aneo.fr>
2023-09-19 11:29:59 +0200

    chore: add test for checking api and core versions

--- src/app/services/versions.service.spec.ts
+++ src/app/services/versions.service.spec.ts
@@ -9,35 +9,30 @@ describe('versions service', () => {
     expect(service).toBeTruthy();
   }); 
 
-  describe('with core version', () => {
-    test('core version is correctly filled', ()=> {
-      const coreServiceVersion = service.setCoreVersion('1.0.1'); 
-      expect(coreServiceVersion).toBeUndefined();
-      service.core?.map(version => expect(version).not.toBeNaN());
+  describe('verify core version', () => {
+    test('core version should only have 3 numbers', () => {
+      service.setCoreVersion('0.1.8.9');
+      expect(service.core?.split('.').length).toBe(3); 
     });
 
     test('core version should only be numbers', () => {
       service.setCoreVersion('nouvelle.version.de.core');
-      service.core?.map(version => expect(version).toBeNaN());
-    });
-
-    test('core version should only have 3 numbers', () => {
-      service.setCoreVersion('0.1.8.9');
-      expect(service.core?.length).toBe(3);
+      expect(service.core).toBe(' version indisponible');
     });
   });
 
-  describe('with api version', () => {
-    test('Api version with 3 numbers', ()=> {
-      const APIServiceVersion = service.setAPIVersion('1.0.1'); 
-      expect(APIServiceVersion).toBeUndefined();
-      service.api?.map(versionApi => expect(versionApi).not.toBeNaN());
+
+  describe('verify API version', () => {
+    test('API version should only have 3 numbers', () => {
+      service.setAPIVersion('0.1.8.9');
+      expect(service.core?.split('.').length).toBe(3); 
     });
 
-    test('core version should be 3 numbers', () => {
+    test('API version should only be numbers', () => {
       service.setAPIVersion('nouvelle.version.de.core');
-      service.api?.map(version => expect(version).toBeNaN());
-    });
+      expect(service.core).toBe(' version indisponible');
+    });  
   });
+
 
 });
--- src/app/services/versions.service.ts
+++ src/app/services/versions.service.ts
@@ -2,34 +2,33 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class VersionsService {
-  core: number[] | null = null;
-  api: number[] | null = null;
+  core: string | null = null;
+  api: string | null = null;
 
 
 
   setCoreVersion(version: string): void {
     const coreNumber = this.#formatVersion(version);
-    this.core = this.#formatVersion(this.#fixVersion(coreNumber));
+    const isInvalidCoreNumber = coreNumber.some(number => Number.isNaN(number));
+    this.core = isInvalidCoreNumber ? ' version indisponible' : this.#fixVersion(coreNumber);
   }
 
   setAPIVersion(version: string): void {
     const APINumber = this.#formatVersion(version);
-    this.core = this.#formatVersion(this.#fixVersion(APINumber));
+    const isInvalidCoreNumber = APINumber.some(number => Number.isNaN(number));
+    this.api = isInvalidCoreNumber ? ' version indisponible' : this.#fixVersion(APINumber);
   }
 
   #formatVersion(version: string): number[] {
     const versionParts = version.split('.');
-    const arr = versionParts.map( versionPart =>  Number(versionPart));
-    return arr;
+    return versionParts.map(versionPart =>  Number(versionPart)); 
   }
   
   #fixVersion(version: number[]): string {
-    // If version has 4 numbers, remove the last one
-
     if (version.length === 4) {
       version.pop();
     }
-
     return version.join('.');
-  }
+  } 
+  
 }
