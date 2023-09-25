905f6fe62aa43339b875b11105a79084ffb0dedf
Yannick BLEY (ANEO) <ybley@aneo.fr>
2023-09-25 15:33:54 +0200

    chore: add tests for defaultDashboardLines configuration

--- src/app/services/default-config-services.spec.ts
+++ src/app/services/default-config-services.spec.ts
@@ -12,5 +12,16 @@ describe('Default config service', () => {
   it('should have the right default theme', () => {
     expect(service.defaultTheme).toBe('indigo-pink');
   });
+  describe(' default dashboard configuration', () => {
+    it('the dashboard lines configuration should display at least 1 line', () => {
+      expect(service.defaultDashboardLines).toHaveLength(1);
+    });
+
+    it('the line by default should contain 3 task statuses', () => {
+      const defaultDashboardLines = service.defaultDashboardLines;
+      const taskStatuses = defaultDashboardLines.map( line => line.taskStatusesGroups); 
+      expect(taskStatuses[0]).toHaveLength(3);
+    });
+  });
 
 }); 
