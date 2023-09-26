79bbca089da18011d59c82877cd89a799f1dd09e
Faust1 <fdewas@aneo.fr>
2023-09-26 17:24:40 +0200

    added doc and tests for table services

--- src/app/services/table-url.service.spec.ts
+++ src/app/services/table-url.service.spec.ts
@@ -1,5 +1,7 @@
+import { TaskSummaryEnumField } from '@aneoconsultingfr/armonik.api.angular';
 import { TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
+import { TaskFilterDefinition } from '@app/tasks/types';
 import { TableURLService } from './table-url.service';
 
 describe('TableURLService', () => {
@@ -8,11 +10,46 @@ describe('TableURLService', () => {
   const mockActivatedRoute = {
     snapshot: {
       queryParamMap: {
-        keys: ['key1', 'key2', 'key3'],
+        keys: [
+          '1-root-1-0',
+          '1-root-16-0',
+          '0-root-13-0',
+          'nextAreIncorrects',
+          '2--12-0',
+          '0-root-13-',
+          '-root-13-0',
+          '0-root--0',
+          'nextHasNoDefinition',
+          '2-root-3-1'
+        ],
         get: jest.fn()
       }
     }
   };
+  const filterDefs: TaskFilterDefinition[] = [
+    {
+      for: 'root',
+      field: TaskSummaryEnumField.TASK_SUMMARY_ENUM_FIELD_SESSION_ID,
+      type: 'string'
+    },
+    {
+      for: 'root',
+      field: TaskSummaryEnumField.TASK_SUMMARY_ENUM_FIELD_TASK_ID,
+      type: 'string',
+    },
+    {
+      for: 'root',
+      field: TaskSummaryEnumField.TASK_SUMMARY_ENUM_FIELD_POD_HOSTNAME,
+      type: 'string'
+    },
+    {
+      for: 'root',
+      field: TaskSummaryEnumField.TASK_SUMMARY_ENUM_FIELD_POD_TTL,
+      type: 'string'
+    }
+  ];
+  const consoleLogSpy = jest.spyOn(console, 'log');
+  consoleLogSpy.mockImplementation(); 
 
   beforeEach(() => {
     service = TestBed.configureTestingModule({
@@ -22,10 +59,14 @@ describe('TableURLService', () => {
       ]
     }).inject(TableURLService);
 
-    mockActivatedRoute.snapshot.queryParamMap.get.mockImplementationOnce((key: 'string') => {
+    mockActivatedRoute.snapshot.queryParamMap.get.mockImplementation((key: string) => {
       const queryParamMap: Record<string, unknown> = {
         'pageIndex': 1,
-        'filters': 'someData'
+        '1-root-1-0': 'someData',
+        '1-root-16-0': 'anotherKindOfData',
+        '0-root-13-0': 'stillData',
+        '2--12-0': 'yetAgainData',
+        'thisOneHas': '{unparsable:data}'
       };
       
       return queryParamMap[key] ?? null;
@@ -45,4 +86,44 @@ describe('TableURLService', () => {
     });
   });
 
+  describe('getQueryParamsFilters', () => {
+    it('should return a FiltersOr of 3 filters', () => {
+      const result = [[
+        {
+          field: 1,
+          operator: 0,
+          value: 'someData',
+          for: 'root'
+        }, 
+        {
+          field: 16,
+          operator: 0,
+          value: 'anotherKindOfData',
+          for: 'root'
+        }
+      ], [
+        {
+          field: 13,
+          operator: 0,
+          value: 'stillData',
+          for: 'root'
+        }]];
+      expect(service.getQueryParamsFilters(filterDefs)).toEqual(result);
+    });
+
+    it('Should print logs in the console in case of incorrect definition', () => {
+      service.getQueryParamsFilters(filterDefs);
+      expect(consoleLogSpy).toHaveBeenCalledWith('Unknown filter', 3);
+    });
+  });
+
+  describe('getQueryParam', () => {
+    it('should parse the parse parameter is set to true', () => {
+      expect(service.getQueryParam('pageIndex')).toEqual(1);
+    });
+  
+    it('should throw SyntaxError when the data is not parsable', () => {
+      expect(() => {service.getQueryParam('thisOneHas');}).toThrow(SyntaxError);
+    });
+  });  
 });
