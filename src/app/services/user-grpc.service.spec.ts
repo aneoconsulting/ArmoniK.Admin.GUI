b1568feba589f1259b9d1e5f7c816540fcaf9dc5
Yannick BLEY (ANEO) <ybley@aneo.fr>
2023-09-26 12:24:23 +0200

    chore: add test to userGrpcService

---
+++ src/app/services/user-grpc.service.spec.ts
@@ -0,0 +1,40 @@
+import { AuthenticationClient, GetCurrentUserResponse } from '@aneoconsultingfr/armonik.api.angular';
+import { TestBed } from '@angular/core/testing';
+import { BehaviorSubject } from 'rxjs';
+import { UserGrpcService } from './user-grpc.service';
+TestBed;
+
+describe('VersionsGrpcService', () => {
+  let service: UserGrpcService;
+  const user = new BehaviorSubject<GetCurrentUserResponse>({
+    user: {
+      username: 'user', 
+      roles: ['admin'], 
+      permissions: ['all']
+
+    }
+  } as unknown as GetCurrentUserResponse).asObservable();
+
+  beforeEach(() => {
+    service = TestBed.configureTestingModule({
+      providers: [
+        UserGrpcService,
+        {provide: AuthenticationClient, useValue: {
+          getCurrentUser: () => {
+            return user;
+          }
+        }}
+      ]
+    }).inject(UserGrpcService);
+  });
+
+  
+  it('should create userGrpcService ', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return an user', () => {
+    expect(service.me$()).toBe(user);
+  });
+
+});
