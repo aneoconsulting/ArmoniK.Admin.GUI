0315f82400080bfa1fd789b04ee003ba32924b47
yannick-aneo <136307285+yannick-aneo@users.noreply.github.com>
2023-09-28 14:29:34 +0200

    chore:  add test to notification.service (#641)
    Co-authored-by: Faust1 <117363666+Faust1-2was-Aneo@users.noreply.github.com>

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -5,19 +5,34 @@ import { NotificationStatus } from '@app/types/notification';
 @Injectable()
 export class NotificationService {
   #snackBar = inject(MatSnackBar);
-
+  /**
+   * Send a message to the function that display the success notification bar and display it in UI.
+   * @param message string
+   */
   success(message: string): void {
     this.#openSnackBar(message, 'Close', 'success');
   }
-
+  /**
+   * Send a message to the function that display the error notification bar and display it in UI.
+   * @param message 
+   */
   error(message: string): void {
     this.#openSnackBar(message, 'Close', 'error');
   }
-
+  /**
+   * Send a message to the function that display the warning notification bar and display it in UI.
+   * @param message String
+   */
   warning(message: string): void {
     this.#openSnackBar(message, 'Close', 'warning');
   }
-
+  /**
+   * 
+   * Display in UI a little notification bar with different statuses and messages for users.
+   * @param message String
+   * @param action String
+   * @param status Object type Notification status 
+   */
   #openSnackBar(message: string, action: string, status: NotificationStatus): void {
     this.#snackBar.open(message, action, {
       duration: 5000,
