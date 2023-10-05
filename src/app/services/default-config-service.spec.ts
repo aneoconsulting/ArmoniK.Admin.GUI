4028cbd7c9985cc2dad3f71941a3063e28b0f34f
Yannick BLEY (ANEO) <ybley@aneo.fr>
2023-10-05 16:57:28 +0200

    chore: finish tests

---
+++ src/app/services/default-config-service.spec.ts
@@ -0,0 +1,110 @@
+import { DefaultConfigService } from './default-config.service';
+
+describe('Default config service', () => {
+  let service: DefaultConfigService; 
+  
+ 
+  it('should create default config service', () => {
+    service = new DefaultConfigService(); 
+    expect(service).toBeTruthy();
+  });
+
+  it('should have the right default theme', () => {
+    expect(service.defaultTheme).toBe('indigo-pink');
+  });
+
+  it('should call defaultExternalServices getter', () => {
+    const spyGetDefaultExternalServices = jest.spyOn(service, 'defaultExternalServices', 'get');
+    service.defaultExternalServices;
+    expect(spyGetDefaultExternalServices).toHaveBeenCalled();
+  });
+
+  it('should call defaultTasks getter', () => {
+    const spyGetDefaultTasks = jest.spyOn(service, 'defaultTasks', 'get');
+    service.defaultTasks;
+    expect(spyGetDefaultTasks).toHaveBeenCalled();
+  });
+
+  it('should call defaultTasksByStatus getter', () => {
+    const spyGetDefaultTasksByStatus = jest.spyOn(service, 'defaultTasksByStatus', 'get');
+    service.defaultTasksByStatus;
+    expect(spyGetDefaultTasksByStatus).toHaveBeenCalled();
+  });
+
+  it('should call defaultPartititons getter', () => {
+    const spyGetDefaultPartitions = jest.spyOn(service, 'defaultPartitions', 'get');
+    service.defaultPartitions;
+    expect(spyGetDefaultPartitions).toHaveBeenCalled();
+  });
+
+  it('should call defaultDashboardSplitLines getter', ()=> {
+    const spyGetDefaultDashboardSplitLines = jest.spyOn(service, 'defaultDashboardSplitLines', 'get');
+    service.defaultDashboardSplitLines;
+    expect(spyGetDefaultDashboardSplitLines).toHaveBeenCalled();
+  });
+
+  it('should call defaultDashboardLines getter', ()=> {
+    const spyGetDefaultDashboardLines = jest.spyOn(service, 'defaultDashboardLines', 'get');
+    service.defaultDashboardLines;
+    expect(spyGetDefaultDashboardLines).toHaveBeenCalled();
+  }); 
+
+  it('should call defaultSessions getter', () => {
+    const spyGetDefaultSessions = jest.spyOn(service, 'defaultSessions', 'get');
+    service.defaultSessions;
+    expect(spyGetDefaultSessions).toHaveBeenCalled();
+  });
+
+  it('should call defaultResults getter', () => {
+    const spyGetDefaultResults = jest.spyOn(service, 'defaultResults', 'get');
+    service.defaultResults;
+    expect(spyGetDefaultResults).toHaveBeenCalled();
+  });
+
+  it('should call defaultApplications getter', () => {
+    const spyGetDefaultApplications = jest.spyOn(service, 'defaultApplications', 'get');
+    service.defaultApplications;
+    expect(spyGetDefaultApplications).toHaveBeenCalled();
+  });
+
+  it('should call defaultSidebar getter', () => {
+    const spyGetDefaultSidebar = jest.spyOn(service, 'defaultSidebar', 'get');
+    service.defaultSidebar;
+    expect(spyGetDefaultSidebar).toHaveBeenCalled();
+  });
+
+  it('should call defaultTasksViewInLogs getter', () => {
+    const spyGetDefaultTasksViewInLogs = jest.spyOn(service, 'defaultTasksViewInLogs', 'get');
+    service.defaultTasksViewInLogs;
+    expect(spyGetDefaultTasksViewInLogs).toHaveBeenCalled();
+  });
+
+  it('should call exportedDefaultConfig getter', () => {
+    const spyGetExportedDefaultConfig = jest.spyOn(service, 'exportedDefaultConfig', 'get');
+    service.exportedDefaultConfig;
+    expect(spyGetExportedDefaultConfig).toHaveBeenCalled();
+  });
+  describe(' default dashboard configuration', () => {
+    it('the dashboard lines configuration should display at least 1 line', () => {
+      expect(service.defaultDashboardLines).toHaveLength(1);
+    });
+
+    it('the line by default should contain 3 task statuses', () => {
+      const defaultDashboardLines = service.defaultDashboardLines;
+      const taskStatuses = defaultDashboardLines.map( line => line.taskStatusesGroups); 
+      expect(taskStatuses[0]).toHaveLength(3);
+    });
+    it('the line by default should have finished, running, error, task statuses by default ', () => {
+      const defaultDashboardLines = service.defaultDashboardLines;
+      const taskStatuses = defaultDashboardLines.map( line => line.taskStatusesGroups); 
+      expect(taskStatuses[0].map(taskStatus => taskStatus.name === 'Finished')).toBeTruthy(); 
+      expect(taskStatuses[0].map(taskStatus => taskStatus.name === 'Running')).toBeTruthy();  
+      expect(taskStatuses[0].map(taskStatus => taskStatus.name === 'Errors')).toBeTruthy();  
+    });
+
+    it('default DashboardSplitLines should return 1 line', ()=> {
+      const defaultDashboardSplitLines :number = service.defaultDashboardSplitLines;
+      expect(defaultDashboardSplitLines).toEqual(1);
+    });
+  });
+}); 
