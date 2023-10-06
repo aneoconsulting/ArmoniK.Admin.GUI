e692d0734a874fa8d018a5f5a2793d621d7d006f
Yannick BLEY (ANEO) <ybley@aneo.fr>
2023-10-06 09:11:22 +0200

    fix: delete useless spec file

--- src/app/services/default-config-service.spec.ts
+++ src/app/services/default-config-service.spec.ts
@@ -3,7 +3,6 @@ import { DefaultConfigService } from './default-config.service';
 describe('Default config service', () => {
   let service: DefaultConfigService; 
   
- 
   it('should create default config service', () => {
     service = new DefaultConfigService(); 
     expect(service).toBeTruthy();
