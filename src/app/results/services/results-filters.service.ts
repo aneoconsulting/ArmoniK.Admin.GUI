f3b6594fff171ae870afd537ce981483116cd387
Faust1 <117363666+Faust1-2was-Aneo@users.noreply.github.com>
2023-10-10 15:37:43 +0200

    chore: added missing filters to results component (#680)

--- src/app/results/services/results-filters.service.ts
+++ src/app/results/services/results-filters.service.ts
@@ -51,7 +51,13 @@ export class ResultsFiltersService {
           value: this.#resultsStatusesService.statuses[Number(status) as ResultStatus],
         };
       }),
-    }];
+    },
+    {
+      for: 'root',
+      field: ResultRawEnumField.RESULT_RAW_ENUM_FIELD_CREATED_AT,
+      type: 'date'
+    }
+  ];
 
   readonly #defaultFilters: ResultRawFiltersOr = this.#defaultConfigService.defaultResults.filters;
 
