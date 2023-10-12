d4aa9eb1a6679248ed4625d15642d5c2ef8f42b0
Faust1 <fdewas@aneo.fr>
2023-10-12 17:03:05 +0200

    chore: tests for edit name line dialog component

---
+++ src/app/dashboard/components/edit-name-line-dialog-component.spec.ts
@@ -0,0 +1,40 @@
+import { TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { EditNameLineDialogComponent } from './edit-name-line-dialog.component';
+
+describe('', () => {
+  let component: EditNameLineDialogComponent;
+
+  const mockMatDialogRef = {
+    close: jest.fn()
+  };
+
+  beforeEach(() => {
+    component = TestBed.configureTestingModule({
+      providers: [
+        EditNameLineDialogComponent,
+        { provide: MatDialogRef, useValue: mockMatDialogRef },
+        { provide: MAT_DIALOG_DATA, useValue: {name: 'name'} }
+      ]
+    }).inject(EditNameLineDialogComponent);
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should init', () => {
+    component.ngOnInit();
+    expect(component.name).toEqual('name');
+  });
+
+  it('should close with result on submit', () => {
+    component.onSubmit('result');
+    expect(mockMatDialogRef.close).toHaveBeenCalledWith({name: 'result'});
+  });
+
+  it('should close on "no" click', () => {
+    component.onNoClick();
+    expect(mockMatDialogRef.close).toHaveBeenCalled();
+  });
+});
