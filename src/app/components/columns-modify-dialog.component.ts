fb218a9fbf80cd299bf3fa96b9618d6c8de36719
Dinesh GUNASEKARAN (ANEO) <dgunasekaran@aneo.fr>
2023-12-05 11:01:03 +0100

    fix: filter disappearing fixed

--- src/app/components/columns-modify-dialog.component.ts
+++ src/app/components/columns-modify-dialog.component.ts
@@ -106,12 +106,10 @@ export class ColumnsModifyDialogComponent<T extends object,O extends object> imp
     if (checked) {
       if (!this.columns.includes(column) && this.data.availableColumns.includes(column)) {
         this.columns.push(column);
-        this.data.availableColumns = this.data.availableColumns.filter(currentColumn => currentColumn !== column);
       }
     } else {
       if(this.columns.includes(column)) {
         this.columns = this.columns.filter(currentColumn => currentColumn !== column);
-        this.data.availableColumns.push(column);
       }
     }
   }
