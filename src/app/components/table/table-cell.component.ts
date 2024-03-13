7ccd529d6d4d78480a469b38e3d395be63e40ad5
Faust1 <fdewas@aneo.fr>
2024-03-13 11:03:04 +0100

    updated tests

--- src/app/components/table/table-cell.component.ts
+++ src/app/components/table/table-cell.component.ts
@@ -78,6 +78,9 @@ export class TableCellComponent<T extends ArmonikData<DataRaw>, K extends RawCol
   }
 
   handleNestedKeys(element: T) {
+    if (element === undefined || element.raw === undefined) {
+      return undefined;
+    }
     const keys = `${this.column.key}`.split('.') as unknown as K[];
     let resultObject: {[key: string]: object} = element.raw as unknown as {[key: string]: object};
     keys.forEach(key => {
