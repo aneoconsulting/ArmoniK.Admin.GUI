51ae0a529238c15f7e0c8419483f76dc6ce1c394
Faust1 <fdewas@aneo.fr>
2024-03-18 09:26:37 +0100

    update table-cell component

--- src/app/components/table/table-cell.component.ts
+++ src/app/components/table/table-cell.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe, NgIf } from '@angular/common';
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { RouterModule } from '@angular/router';
@@ -30,12 +30,16 @@ import { TableInspectObjectComponent } from './table-inspect-object.component';
     MatCheckboxModule,
   ]
 })
-export class TableCellComponent<T extends ArmonikData<DataRaw>, K extends RawColumnKey, S extends Status> implements OnInit, OnDestroy{  
+export class TableCellComponent<T extends ArmonikData<DataRaw>, K extends RawColumnKey, S extends Status> implements OnDestroy{  
   @Input({ required: true }) column: TableColumn<K>;
   @Input({ required: true }) value$: Subject<DataRaw>;
   @Input({ required: true }) set element(entry: T) {
     this._element = entry;
     this._value = this.handleNestedKeys(entry);
+    this.value$.subscribe((entry: DataRaw) => {
+      this._element.raw = entry;
+      this._value = this.handleNestedKeys(this._element);
+    });
   }
 
   @Input({ required: false }) statusesService: StatusesServiceI<S>;
@@ -46,15 +50,6 @@ export class TableCellComponent<T extends ArmonikData<DataRaw>, K extends RawCol
 
   private _value: unknown;
   private _element: T;
-
-  ngOnInit(): void {
-    this.value$.subscribe((entry: DataRaw) => {
-      console.log('updating line', entry['sessionId' as keyof DataRaw]);
-      this._element.raw = entry;
-      // console.log(entry['sessionId' as keyof DataRaw], entry, this.element.raw);
-      this._value = this.handleNestedKeys(this._element);
-    });
-  }
 
   ngOnDestroy(): void {
     this.value$.unsubscribe();
