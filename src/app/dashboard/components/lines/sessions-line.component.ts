daddb1d4cc71abc20900fbcc3e6514a7d5038a46
Faust1 <fdewas@aneo.fr>
2024-04-05 11:29:57 +0200

    feat: sessions line dashboard

---
+++ src/app/dashboard/components/lines/sessions-line.component.ts
@@ -0,0 +1,62 @@
+import { AfterViewInit, Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SessionsTableComponent } from '@app/sessions/components/table.component';
+import { SessionsFiltersService } from '@app/sessions/services/sessions-filters.service';
+import { SessionsIndexService } from '@app/sessions/services/sessions-index.service';
+import { SessionsStatusesService } from '@app/sessions/services/sessions-statuses.service';
+import { SessionRawColumnKey, SessionRawFilters, SessionRawListOptions } from '@app/sessions/types';
+import { TasksFiltersService } from '@app/tasks/services/tasks-filters.service';
+import { TasksStatusesService } from '@app/tasks/services/tasks-statuses.service';
+import { DashboardLineTableComponent } from '@app/types/components/dashboard-line-table';
+import { FiltersToolbarComponent } from '@components/filters/filters-toolbar.component';
+import { TableActionsToolbarComponent } from '@components/table-actions-toolbar.component';
+import { NotificationService } from '@services/notification.service';
+
+@Component({
+  selector: 'app-dashboard-sessions-line',
+  templateUrl: './sessions-line.component.html',
+  standalone: true,
+  styles: [`
+app-table-actions-toolbar {
+  flex-grow: 1;
+}
+.filters {
+  height: auto;
+  min-height: 64px;
+  padding: 1rem;
+}
+  `],
+  providers: [
+    MatSnackBar,
+    NotificationService,
+    SessionsFiltersService,
+    SessionsIndexService,
+    SessionsStatusesService,
+    TasksStatusesService,
+    TasksFiltersService,
+  ],
+  imports: [
+    MatToolbarModule,
+    TableActionsToolbarComponent,
+    FiltersToolbarComponent,
+    SessionsTableComponent,
+    MatIconModule,
+  ],
+})
+export class SessionsLineComponent extends DashboardLineTableComponent<SessionRawColumnKey, SessionRawListOptions, SessionRawFilters> implements OnInit, AfterViewInit, OnDestroy {
+  readonly indexService = inject(SessionsIndexService);
+
+  ngOnInit(): void {
+    this.initLineEnvironment();
+  }
+
+  ngAfterViewInit(): void {
+    this.mergeSubscriptions();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe();
+  }
+}
