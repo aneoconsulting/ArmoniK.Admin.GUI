073ed636f2cee114d925e46431031cdaea124b4c
Faustin Dewas <117363666+fdewas-aneo@users.noreply.github.com>
2024-04-08 14:08:25 +0200

    feat: column header (#1019)
    Co-authored-by: ngruelaneo <100275739+ngruelaneo@users.noreply.github.com>

---
+++ src/app/components/table/table-column-header.component.spec.ts
@@ -0,0 +1,59 @@
+import { TestBed } from '@angular/core/testing';
+import { SessionRawColumnKey } from '@app/sessions/types';
+import { TableColumn } from '@app/types/column.type';
+import { IconsService } from '@services/icons.service';
+import { TableColumnHeaderComponent } from './table-column-header.component';
+
+describe('TableColumnHeaderComponent', () => {
+  let component: TableColumnHeaderComponent<SessionRawColumnKey>;
+
+  const initColumn: TableColumn<SessionRawColumnKey> = {
+    type: 'count',
+    key: 'count',
+    name: 'Tasks by Status',
+    sortable: false,
+  };
+
+  beforeEach(() => {
+    component = TestBed.configureTestingModule({
+      providers: [
+        TableColumnHeaderComponent,
+        IconsService,
+      ]
+    }).inject(TableColumnHeaderComponent);
+    component.column = initColumn;
+  });
+  
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should correctly set name', () => {
+    expect(component.name).toEqual(initColumn.name);
+  });
+
+  it('should correctly set icon', () => {
+    expect(component.icon).toEqual(component.iconService.getIcon('tune'));
+  });
+
+  it('should correctly set type', () => {
+    expect(component.type).toEqual(initColumn.type);
+  });
+
+  it('should set "raw" as the default type', () => {
+    component.column = { ...initColumn, type: undefined };
+    expect(component.type).toEqual('raw');
+  });
+
+  it('should emit on toggle all rows', () => {
+    const spy = jest.spyOn(component.rowsSelectionChange, 'emit');
+    component.onToggleAllRows();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should emit on personnalize tasks by status', () => {
+    const spy = jest.spyOn(component.statusesChange, 'emit');
+    component.onPersonnalizeStatuses();
+    expect(spy).toHaveBeenCalled();
+  });
+});
