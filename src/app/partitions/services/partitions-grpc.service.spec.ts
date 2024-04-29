2d76adf38762c8c073d15d65da25d76823e30242
Faust1 <fdewas@aneo.fr>
2024-04-29 11:13:58 +0200

    chore: tests for partitions services and components

---
+++ src/app/partitions/services/partitions-grpc.service.spec.ts
@@ -0,0 +1,192 @@
+import { FilterArrayOperator, FilterNumberOperator, FilterStatusOperator, FilterStringOperator, GetPartitionRequest, ListPartitionsRequest, PartitionRawEnumField, PartitionsClient } from '@aneoconsultingfr/armonik.api.angular';
+import { TestBed } from '@angular/core/testing';
+import { ListOptionsSort } from '@app/types/options';
+import { UtilsService } from '@services/utils.service';
+import { PartitionsFiltersService } from './partitions-filters.service';
+import { PartitionsGrpcService } from './partitions-grpc.service';
+import { PartitionRaw, PartitionRawFilters, PartitionRawListOptions } from '../types';
+
+describe('PartitionsGrpcService', () => {
+  let service: PartitionsGrpcService;
+
+  const filters: PartitionRawFilters = [
+    [
+      {
+        field: PartitionRawEnumField.PARTITION_RAW_ENUM_FIELD_ID,
+        for: 'root',
+        operator: FilterStringOperator.FILTER_STRING_OPERATOR_EQUAL,
+        value: 'partitionId'
+      },
+      {
+        field: PartitionRawEnumField.PARTITION_RAW_ENUM_FIELD_PARENT_PARTITION_IDS,
+        for: 'root',
+        operator: FilterArrayOperator.FILTER_ARRAY_OPERATOR_CONTAINS,
+        value: '12323981203'
+      },
+    ],
+    [
+      {
+        field: PartitionRawEnumField.PARTITION_RAW_ENUM_FIELD_POD_MAX,
+        for: 'root',
+        operator: FilterNumberOperator.FILTER_NUMBER_OPERATOR_LESS_THAN,
+        value: 100
+      }
+    ]
+  ];
+
+  const options: PartitionRawListOptions = {
+    pageIndex: 2,
+    pageSize: 10,
+    sort: {
+      active: 'id',
+      direction: 'asc'
+    }
+  };
+
+  const mockPartitionsFiltersService = {
+    filtersDefinitions: [
+      {
+        for: 'root',
+        field: PartitionRawEnumField.PARTITION_RAW_ENUM_FIELD_ID,
+        type: 'string'
+      },
+      {
+        for: 'root',
+        field: PartitionRawEnumField.PARTITION_RAW_ENUM_FIELD_PARENT_PARTITION_IDS,
+        type: 'array',
+      },
+      {
+        for: 'root',
+        field: PartitionRawEnumField.PARTITION_RAW_ENUM_FIELD_POD_MAX,
+        type: 'number'
+      },
+      {
+        for: 'root',
+        field: PartitionRawEnumField.PARTITION_RAW_ENUM_FIELD_POD_RESERVED,
+        type: 'status'
+      }
+    ],
+  };
+
+  const mockPartitionsClient = {
+    listPartitions: jest.fn(),
+    getPartition: jest.fn(),
+  };
+
+  beforeEach(() => {
+    service = TestBed.configureTestingModule({
+      providers: [
+        PartitionsGrpcService,
+        UtilsService,
+        { provide: PartitionsFiltersService, useValue: mockPartitionsFiltersService },
+        { provide: PartitionsClient, useValue: mockPartitionsClient }
+      ]
+    }).inject(PartitionsGrpcService);
+  });
+
+  it('should create', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should list partitions', () => {
+    service.list$(options, filters);
+    expect(mockPartitionsClient.listPartitions).toHaveBeenCalledWith(new ListPartitionsRequest({
+      page: options.pageIndex,
+      pageSize: options.pageSize,
+      sort: {
+        direction: 1,
+        field: {
+          partitionRawField: {
+            field: PartitionRawEnumField.PARTITION_RAW_ENUM_FIELD_ID
+          }
+        }
+      },
+      filters: {
+        or: [
+          {
+            and: [
+              {
+                field: {
+                  partitionRawField: {
+                    field: PartitionRawEnumField.PARTITION_RAW_ENUM_FIELD_ID
+                  }
+                },
+                filterString: {
+                  operator: FilterStringOperator.FILTER_STRING_OPERATOR_EQUAL,
+                  value: 'partitionId'
+                }
+              },
+              {
+                field: {
+                  partitionRawField: {
+                    field: PartitionRawEnumField.PARTITION_RAW_ENUM_FIELD_PARENT_PARTITION_IDS
+                  }
+                },
+                filterArray: {
+                  operator: FilterArrayOperator.FILTER_ARRAY_OPERATOR_CONTAINS,
+                  value: '12323981203'
+                }
+              },
+            ]
+          },
+          {
+            and: [
+              {
+                field: {
+                  partitionRawField: {
+                    field: PartitionRawEnumField.PARTITION_RAW_ENUM_FIELD_POD_MAX
+                  }
+                },
+                filterNumber: {
+                  operator: FilterNumberOperator.FILTER_NUMBER_OPERATOR_LESS_THAN,
+                  value: '100'
+                }
+              }
+            ]
+          }
+        ]
+      }
+    }));
+  });
+
+  it('should throw on invalid filters', () => {
+    const filters: PartitionRawFilters = [[{
+      field: PartitionRawEnumField.PARTITION_RAW_ENUM_FIELD_POD_RESERVED,
+      for: 'root',
+      operator: FilterStatusOperator.FILTER_STATUS_OPERATOR_EQUAL,
+      value: 0
+    }]];
+    expect(() => service.list$(options, filters)).toThrow('Type status not supported');
+  });
+
+  it('should have a default sort field direction', () => {
+    const options: PartitionRawListOptions = {
+      pageIndex: 10,
+      pageSize: 10,
+      sort: {
+        active: null,
+        direction: 'asc'
+      } as unknown as ListOptionsSort<PartitionRaw>
+    };
+    service.list$(options, [[]]);
+    expect(mockPartitionsClient.listPartitions).toHaveBeenCalledWith(new ListPartitionsRequest({
+      page: options.pageIndex,
+      pageSize: options.pageSize,
+      sort: {
+        direction: 1,
+        field: {
+          partitionRawField: {
+            field: PartitionRawEnumField.PARTITION_RAW_ENUM_FIELD_ID
+          }
+        }
+      },
+      filters: {}
+    }));
+  });
+
+  it('should get a partition', () => {
+    const id = 'partitionId';
+    service.get$(id);
+    expect(mockPartitionsClient.getPartition).toHaveBeenCalledWith(new GetPartitionRequest({ id }));
+  });
+});
---
+++ src/app/partitions/show.component.spec.ts
@@ -0,0 +1,141 @@
+import { GetPartitionResponse } from '@aneoconsultingfr/armonik.api.angular';
+import { TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject, Observable, of, throwError } from 'rxjs';
+import { FiltersService } from '@services/filters.service';
+import { IconsService } from '@services/icons.service';
+import { NotificationService } from '@services/notification.service';
+import { ShareUrlService } from '@services/share-url.service';
+import { PartitionsGrpcService } from './services/partitions-grpc.service';
+import { ShowComponent } from './show.component';
+import { PartitionRaw } from './types';
+
+describe('ShowComponent', () => {
+  let component: ShowComponent;
+
+  const mockNotificationService = {
+    success: jest.fn(),
+    error: jest.fn(),
+  };
+
+  const mockShareUrlService = {
+    generateSharableURL: jest.fn(),
+  };
+
+  const paramId = 'paramId-12345';
+  const mockActivatedRoute = {
+    params: new BehaviorSubject({
+      id: paramId,
+    }),
+  };
+
+  const returnedPartition = {
+    id: 'partitionId-12345',
+    options: {
+      partitionId: 'partitionId'
+    }
+  } as unknown as PartitionRaw;
+  const mockPartitionsGrpcService = {
+    get$: jest.fn((): Observable<unknown> => of({partition: returnedPartition} as GetPartitionResponse)),
+  };
+
+  beforeEach(() => {
+    component = TestBed.configureTestingModule({
+      providers: [
+        ShowComponent,
+        IconsService,
+        FiltersService,
+        { provide: NotificationService, useValue: mockNotificationService },
+        { provide: ShareUrlService, useValue: mockShareUrlService },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: PartitionsGrpcService, useValue: mockPartitionsGrpcService }
+      ]
+    }).inject(ShowComponent);
+    component.ngOnInit();
+    component.ngAfterViewInit();
+  });
+
+  it('should create', () => {
+    expect(component).toBeDefined();
+  });
+
+  describe('Initialisation', () => {
+    it('should set id', () => {
+      expect(component.id).toEqual(paramId);
+    });
+
+    it('should set sharableURL', () => {
+      expect(mockShareUrlService.generateSharableURL).toHaveBeenCalled();
+    });
+  });
+
+  it('should get page icon', () => {
+    expect(component.getPageIcon('partitions')).toEqual('donut_small');
+  });
+
+  it('should get icons', () => {
+    expect(component.getIcon('refresh')).toEqual('refresh');
+  });
+
+  it('should refresh', () => {
+    const spy = jest.spyOn(component.refresh, 'next');
+    component.onRefresh();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  describe('Getting data', () => {
+    it('should fetch data on refresh', () => {
+      component.refresh.next();
+      expect(mockPartitionsGrpcService.get$).toHaveBeenCalledWith(paramId);
+    });
+
+    it('should update data on success', () => {
+      const spy = jest.spyOn(component.data$, 'next');
+      component.refresh.next();
+      expect(component.data).toEqual(returnedPartition);
+      expect(spy).toHaveBeenCalledWith(returnedPartition);
+    });
+
+    it('should not update data if there is none', () => {
+      mockPartitionsGrpcService.get$.mockImplementationOnce(() => of({}));
+      const spy = jest.spyOn(component.data$, 'next');
+      component.refresh.next();
+      expect(spy).not.toHaveBeenCalled();
+    });
+
+    it('should catch errors', () => {
+      jest.spyOn(console, 'error').mockImplementation(() => {});
+      mockPartitionsGrpcService.get$.mockImplementationOnce(() => throwError(() => new Error()));
+      const spy = jest.spyOn(component, 'handleError');
+      component.refresh.next();
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe('Handle errors', () => {
+    it('should log errors', () => {
+      const errorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+      component.handleError(new Error());
+      expect(errorSpy).toHaveBeenCalled();
+    });
+
+    it('should notify the error', () => {
+      component.handleError(new Error());
+      expect(mockNotificationService.error).toHaveBeenCalledWith('Could not retrieve data.');
+    });
+  });
+
+  describe('notifications', () => {
+    it('should notify on success', () => {
+      const notification = 'message';
+      component.success(notification);
+      expect(mockNotificationService.success).toHaveBeenCalledWith(notification);
+    });
+
+    it('should notify on error', () => {
+      const error = 'error message';
+      component.error(error);
+      expect(mockNotificationService.error).toHaveBeenCalledWith(error);
+    });
+  });
+});
