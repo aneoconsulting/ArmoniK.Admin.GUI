7b06e2bf28d13f606e5c8fe6aeeb8746ec3b2fc2
Faust1 <fdewas@aneo.fr>
2024-05-17 14:12:51 +0200

    feat: user documentation

---
+++ .docs/components/content/horizontal-area.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="flex w-full justify-start mt-2">
+    <div class="pr-5 w-2/3">
+      <ContentSlot :use="$slots.left" />
+    </div>
+    <div class="flex pl-5 w-1/3">
+      <ContentSlot :use="$slots.right" />
+    </div>
+  </section>
+</template>
---
+++ .docs/nuxt.config.ts
@@ -0,0 +1,25 @@
+const baseURL = process.env.NODE_ENV === 'production' ? '/ArmoniK/' : '/'
+
+export default defineNuxtConfig({
+  app: {
+    baseURL,
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/ico',
+          href: `${baseURL}favicon.ico`
+        }
+      ]
+    }
+  },
+
+  extends: '@aneoconsultingfr/armonik-docs-theme',
+
+  runtimeConfig: {
+    public: {
+      siteName: 'ArmoniK.Admin.GUI',
+      siteDescription: 'Monitor and manage an ArmoniK cluster'
+    }
+  }
+})
