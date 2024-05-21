dcaf4a4cbf2d68eb154a1d3e44f26f437a2f0ae4
Faust1 <fdewas@aneo.fr>
2024-05-21 17:01:08 +0200

    update docs

--- .docs/app.config.ts
+++ .docs/app.config.ts
@@ -1,39 +1,14 @@
-// https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: 'ArmoniK',
+    description: 'The next-gen orchestrator',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
+      github: 'aneoconsulting/ArmoniK'
     },
     github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
-    },
-    aside: {
-      level: 0,
-      collapsed: false,
-      exclude: []
-    },
-    main: {
-      padded: true,
-      fluid: true
-    },
-    header: {
-      logo: true,
-      showLinkIcon: true,
-      exclude: [],
-      fluid: true
+      dir: '.docs/content',
+      repo: 'ArmoniK',
+      owner: 'aneoconsulting'
     }
   }
 })
