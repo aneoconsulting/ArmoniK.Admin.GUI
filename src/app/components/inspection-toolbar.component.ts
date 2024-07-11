f31324282256b15a7dbc9c19cd6f8c0018f30238
Faust1 <fdewas@aneo.fr>
2024-07-11 16:45:37 +0200

    feat: inspection toolbar

---
+++ src/app/components/inspection-toolbar.component.ts
@@ -0,0 +1,48 @@
+import { Component, Output, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Subject } from 'rxjs';
+import { IconsService } from '@services/icons.service';
+
+@Component({
+  selector: 'app-inspection-toolbar',
+  templateUrl: 'inspection-toolbar.component.html',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  providers: [
+    IconsService
+  ],
+  styles: [`
+  section {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%
+  }
+  
+  mat-toolbar {
+    justify-content: center;
+    gap: 1rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+  }
+
+  mat-toolbar-row {
+    height: fit-content;
+  }  
+  `]
+})
+export class InspectionToolbarComponent {
+  private readonly iconsService = inject(IconsService);
+
+  @Output() refresh = new Subject<void>();
+
+  getIcon(name: string) {
+    return this.iconsService.getIcon(name);
+  }
+}
