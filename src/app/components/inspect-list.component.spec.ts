dd7829a4cd501e366d1ef4a5d511c601a83caa81
Faust1 <fdewas@aneo.fr>
2024-07-12 16:38:21 +0200

    chore: remove iconsService from tests

--- src/app/components/inspect-list.component.spec.ts
+++ src/app/components/inspect-list.component.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-import { IconsService } from '@services/icons.service';
 import { InspectListComponent } from './inspect-list.component';
 
 describe('InspectListComponent', () => {
@@ -9,8 +8,7 @@ describe('InspectListComponent', () => {
   beforeEach(() => {
     component = TestBed.configureTestingModule({
       providers: [
-        InspectListComponent,
-        IconsService,
+        InspectListComponent
       ]
     }).inject(InspectListComponent);
     component.list = list;
