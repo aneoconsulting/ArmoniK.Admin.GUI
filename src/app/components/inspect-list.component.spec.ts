ec11fadad37d86ba95b7797a73c1743b787a8296
fdewas-aneo <fdewas@aneo.fr>
2024-08-07 16:32:18 +0200

    feat: see all button for inspection lists

--- src/app/components/inspect-list.component.spec.ts
+++ src/app/components/inspect-list.component.spec.ts
@@ -4,6 +4,12 @@ import { InspectListComponent } from './inspect-list.component';
 describe('InspectListComponent', () => {
   let component: InspectListComponent;
   const list = ['item1', 'item2', 'item3'];
+  const queryParams = '0-root-1-0';
+  const finalParams = {
+    '0-root-1-0': list[0],
+    '1-root-1-0': list[1],
+    '2-root-1-0': list[2]
+  };
 
   beforeEach(() => {
     component = TestBed.configureTestingModule({
@@ -12,6 +18,7 @@ describe('InspectListComponent', () => {
       ]
     }).inject(InspectListComponent);
     component.list = list;
+    component.queryParams = queryParams;
   });
 
   it('should create', () => {
@@ -31,6 +38,26 @@ describe('InspectListComponent', () => {
     it('should update list if a new one is provided', () => {
       component.list = [];
       expect(component.list).toEqual([]);
+    });
+  });
+
+  describe('setting queryParams', () => {
+    it('should create a correct Params object', () => {
+      expect(component.queryParams).toEqual(finalParams);
+    });
+
+    it('should not update queryParams if it is undefined', () => {
+      component.queryParams = undefined;
+      expect(component.queryParams).toEqual(finalParams);
+    });
+    
+    it('should update queryParams if a new one is provided', () => {
+      component.queryParams = '0-options-2-0';
+      expect(component.queryParams).toEqual({
+        '0-options-2-0': list[0],
+        '1-options-2-0': list[1],
+        '2-options-2-0': list[2]
+      });
     });
   });
 });
