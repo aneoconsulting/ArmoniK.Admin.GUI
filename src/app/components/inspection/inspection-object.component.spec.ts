951a55f64b8d777ad7751bde24a92f568f49f3fa
Faustin <fdewas@aneo.fr>
2024-11-15 16:01:40 +0100

    test: add

--- src/app/components/inspection/inspection-object.component.spec.ts
+++ src/app/components/inspection/inspection-object.component.spec.ts
@@ -1,7 +1,12 @@
 import { TaskStatus } from '@aneoconsultingfr/armonik.api.angular';
 import { TaskRaw } from '@app/tasks/types';
 import { Field } from '@app/types/column.type';
+import { DataRaw } from '@app/types/data';
 import { InspectionObjectComponent } from './inspection-object.component';
+
+function findField<T extends DataRaw>(key: string, fields: Field<T>[]) {
+  return fields.find(field => field.key === key);
+}
 
 describe('InspectionObjectComponent', () => {
   const component = new InspectionObjectComponent<TaskRaw, TaskStatus>();
@@ -10,6 +15,11 @@ describe('InspectionObjectComponent', () => {
     id: 'taskId',
     options: {
       applicationName: 'string',
+    },
+    statusMessage: 'some message',
+    output: {
+      error: 'error message',
+      success: false,
     }
   } as TaskRaw;
 
@@ -25,13 +35,17 @@ describe('InspectionObjectComponent', () => {
       link: 'sessions'
     },
     {
+      key: 'output',
+      type: 'output'
+    },
+    {
+      key: 'statusMessage',
+      type: 'message'
+    },
+    {
       key: 'options',
       type: 'object'
     },
-    {
-      key: 'createdAt',
-      type: 'date'
-    }
   ];
 
   const statuses: Record<TaskStatus, string> = {
@@ -65,11 +79,19 @@ describe('InspectionObjectComponent', () => {
     it('should set data keys as fields if none are provided', () => {
       component.fields = [];
       component.data = data;
-      expect(component.fields).toEqual([{ key: 'id' }, { key: 'options' }]);
+      expect(component.fields).toEqual([{ key: 'id' }, { key: 'options' }, { key: 'output' }, { key: 'statusMessage' }]);
     });
   });
 
   it('should get an object', () => {
-    expect(component.getObject(fields[2])).toEqual(data.options);
+    expect(component.getObject(findField('options', fields)!)).toEqual(data.options);
+  });
+
+  it('should get the output error', () => {
+    expect(component.getError(findField('output', fields)!)).toEqual(data.output?.error);
+  });
+
+  it('should get the message', () => {
+    expect(component.getMessage(findField('statusMessage', fields)!)).toEqual(data.statusMessage);
   });
 });
