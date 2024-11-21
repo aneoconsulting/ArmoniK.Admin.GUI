9243cb50c56c742b740d4ee41abbb94aea98dbc6
Faustin <fdewas@aneo.fr>
2024-11-21 10:24:47 +0100

    chore(ui): responsive header

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,5 +1,4 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -17,8 +16,7 @@ import { NavigationService } from '@services/navigation.service';
 import { StorageService } from '@services/storage.service';
 import { UserService } from '@services/user.service';
 import { VersionsService } from '@services/versions.service';
-import { Observable, Subscription, interval } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Subscription, interval, map, shareReplay } from 'rxjs';
 import { ChangeLanguageButtonComponent } from './change-language-button.component';
 import { ExternalServicesComponent } from './external-services/external-services.component';
 import { ThemeSelectorComponent } from './theme-selector.component';
@@ -27,54 +25,13 @@ import pkg from '../../../../package.json';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styles: [`
-.sidenav-container {
-  height: calc(100% - 64px);
-}
-
-.sidenav {
-  width: 200px;
-}
-
-.sidenav .mat-toolbar {
-  background: inherit;
-}
-
-.mat-toolbar.mat-primary {
-  position: sticky;
-  top: 0;
-  z-index: 2;
-}
-
-.navbar-item-selected {
-  background-color: rgba(0, 0, 0, 0.2);
-}
-
-.spacer {
-  flex: 1 1 auto;
-}
-
-.greeting {
-  font-weight: normal;
-}
-
-.environment {
-  background: white;
-  padding: 0 1rem;
-  border-radius: 0.25rem;
-}
-
-main {
-  padding: 20px 50px;
-}
-  `],
+  styleUrl: 'navigation.component.css',
   standalone: true,
   providers: [
     StorageService,
     IconsService,
   ],
   imports: [
-    AsyncPipe,
     RouterModule,
     ThemeSelectorComponent,
     MatToolbarModule,
@@ -107,7 +64,13 @@ export class NavigationComponent implements OnInit, OnDestroy {
   coreVersion = this.versionsService.core;
   settingsItem = $localize`Settings`;
 
-  checkGreetingsSubscription: Subscription;
+  private readonly _isHandset = signal(false);
+
+  get isHandset() {
+    return this._isHandset();
+  }
+
+  private readonly subscriptions = new Subscription();
 
   private readonly _greetings = signal<string>('');
 
@@ -121,21 +84,16 @@ export class NavigationComponent implements OnInit, OnDestroy {
 
   sidebar = this.navigationService.currentSidebar;
   sideBarOpened = true;
-  
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
 
   ngOnInit(): void {
     this.sideBarOpened = this.navigationService.restoreSideBarOpened();
     this.verifyGreetings();
-    this.checkGreetingsSubscription = interval(60000).subscribe(() => this.verifyGreetings());
+    this.subscriptions.add(interval(60000).subscribe(() => this.verifyGreetings()));
+    this.subscriptions.add(this.setIsHandSet());
   }
 
   ngOnDestroy(): void {
-    this.checkGreetingsSubscription.unsubscribe();
+    this.subscriptions.unsubscribe();
   }
 
   getVersion(): string {
@@ -144,6 +102,16 @@ export class NavigationComponent implements OnInit, OnDestroy {
 
   getIcon(name: string): string {
     return this.iconsService.getIcon(name);
+  }
+
+  private setIsHandSet() {
+    return this.breakpointObserver.observe(Breakpoints.Handset)
+      .pipe(
+        map(result => result.matches),
+        shareReplay()
+      ).subscribe((data) => {
+        this._isHandset.set(data);
+      });
   }
 
   private verifyGreetings() {
