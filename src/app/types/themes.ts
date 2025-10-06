d971eab646ee254dbd3eab0daaec2dbcfb9bd6db
Faustin <fdewas@aneo.fr>
2025-10-06 12:04:11 +0200

    chore: adress sonarqube comments

--- src/app/types/themes.ts
+++ src/app/types/themes.ts
@@ -3,5 +3,5 @@ export const ALL_THEMES = ['light-blue', 'light-pink', 'dark-green', 'dark-purpl
 export type Theme = typeof ALL_THEMES[number];
 
 export function isTheme(value: string): value is Theme {
-  return ALL_THEMES.includes(value as Theme);
+  return ALL_THEMES.includes(value);
 }
