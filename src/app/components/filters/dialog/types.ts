2a7b893299116f01cbdebd6b3e1fb3ed5ce93ef3
Faustin <fdewas@aneo.fr>
2025-11-18 10:19:46 +0100

    doc: filter dialog types

--- src/app/components/filters/dialog/types.ts
+++ src/app/components/filters/dialog/types.ts
@@ -2,13 +2,28 @@ import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { FilterFor } from '@app/types/filter-definition';
 import { FilterFieldValue, FiltersEnums, FiltersOptionsEnums } from '@app/types/filters';
 
-
+/**
+ * Array of FormFiltersAnd. Represents an "Or" filter.
+ */
 export type FormFiltersOr<F extends FiltersEnums, O extends FiltersOptionsEnums | null = null> = FormArray<FormFiltersAnd<F, O>>;
 
+/**
+ * Array of FormFilter. Represents an "And" filter.
+ */
 export type FormFiltersAnd<F extends FiltersEnums, O extends FiltersOptionsEnums | null> = FormArray<FormFilter<F, O>>;
 
+/**
+ * Angular FormGroup allowing to use in a more simplier way the forms from FormFilterType.
+ */
 export type FormFilter<F extends FiltersEnums, O extends FiltersOptionsEnums | null = null> = FormGroup<FormFilterType<F, O>>;
 
+/**
+ * Type containing all forms information about a filter.
+ * - for: FilterFor<FiltersEnum, FiltersOptionsEnums> | null
+ * - field: FilterFieldValue<FiltersEnum, FiltersOptionsEnums>
+ * - value: FilterInputValue
+ * - operator: number | null
+ */
 export type FormFilterType<F extends FiltersEnums, O extends FiltersOptionsEnums | null = null> = {
   for: FormControl<FilterFor<F, O> | null>;
   field: FormControl<FilterFieldValue<F, O>>;
@@ -16,4 +31,7 @@ export type FormFilterType<F extends FiltersEnums, O extends FiltersOptionsEnums
   operator: FormControl<number | null>;
 };
 
+/**
+ * Every possible type for a filter input value
+ */
 export type FilterInputValue = string | number | Date | boolean | null;
