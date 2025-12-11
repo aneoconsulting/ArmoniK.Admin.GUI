6bea3a77aa2515aef78bcde9678e11785455762f
Faustin Dewas <117363666+fdewas-aneo@users.noreply.github.com>
2025-12-11 10:33:37 +0100

    chore: relocate navigation edit (#1323)

---
+++ src/app/components/navigation/add-sidebar-item-dialog/add-sidebar-item.dialog.component.ts
@@ -0,0 +1,54 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { Sidebar } from '@app/types/navigation';
+import { AutoCompleteComponent } from '@components/auto-complete.component';
+import { NavigationService } from '@services/navigation.service';
+import { AddSideBarItemDialogResult } from './types';
+
+@Component({
+  selector: 'app-add-sidebar-item-dialog',
+  templateUrl: 'add-sidebar-item.dialog.component.html',
+  styleUrl: 'add-sidebar-item.dialog.component.scss',
+  imports: [
+    MatDialogModule,
+    AutoCompleteComponent,
+    MatButtonModule,
+  ]
+})
+export class AddSideBarItemDialogComponent implements OnInit {
+  private readonly navigationService = inject(NavigationService);
+
+  availableSidebars: Sidebar[] = [];
+  selectedItem: Sidebar;
+  
+  constructor(private readonly dialogRef: MatDialogRef<AddSideBarItemDialogComponent, AddSideBarItemDialogResult>) {}
+
+  ngOnInit(): void {
+    this.availableSidebars = this.navigationService.sidebarItems
+      .filter(sidebar => !this.navigationService.currentSidebar.includes(sidebar))
+      .map(sidebar => sidebar.id);
+    
+    if (!this.availableSidebars.includes('divider')) {
+      this.availableSidebars.push('divider');
+    }
+
+    if (this.availableSidebars.length === 1) {
+      this.selectedItem = 'divider';
+    }
+  }
+
+  updateSelectedItem(item: string) {
+    this.selectedItem = item as Sidebar;
+  }
+
+  submit() {
+    this.dialogRef.close({
+      item: this.selectedItem,
+    });
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+}
